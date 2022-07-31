e5a3b0fc6d16ef29c089ff9aba58ac309519dd6a
Harsohail Brar <harsohail.brar1@gmail.com>
2022-07-31 12:20:36 -0700

    add omnes font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        omnes: ["Omnes", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
