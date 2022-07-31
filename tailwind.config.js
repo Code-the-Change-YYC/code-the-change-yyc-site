9167a50c590b66732f6cfd7125972e986119ac74
Harsohail Brar <harsohail.brar1@gmail.com>
2022-07-31 12:36:29 -0700

    font api call over https

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        omnes: ["Omnes", "sans-serif"],
+        omnes: ["Omnes", ...defaultTheme.fontFamily.sans],
       },
     },
   },
