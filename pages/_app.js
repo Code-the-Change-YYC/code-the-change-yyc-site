161c48fbcf68691fc345bce2c9a4d9eb56628c81
Harsohail Brar <harsohail.brar1@gmail.com>
2022-08-03 17:46:50 -0700

    set up head

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
+import Head from "next/head";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <div className="font-omnes">
+      <Head>
+        <link rel="shortcut icon" href="logo.png" />
+        <title>Code the Change YYC</title>
+      </Head>
       <Component {...pageProps} />
     </div>
   );
