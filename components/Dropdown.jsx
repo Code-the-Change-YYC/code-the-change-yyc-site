447326b79993a7b7e5ce2664c671f35963437cc8
Harsohail Brar <harsohail.brar1@gmail.com>
2022-08-03 22:35:53 -0700

    arrow size constant

--- components/Dropdown.jsx
+++ components/Dropdown.jsx
@@ -2,12 +2,14 @@ import { Menu } from "@headlessui/react";
 import { FaAngleDown } from "react-icons/fa";
 import Fade from "./Fade";
 
+const SIZE = 20;
+
 const Dropdown = ({ title, titleClasses, children }) => {
   return (
     <Menu as="div" className="relative inline-block text-left">
       <Menu.Button className={`inline-flex w-full justify-center items-center ${titleClasses}`}>
         <span className="mr-1">{title}</span>
-        <FaAngleDown width={20} height={20} />
+        <FaAngleDown width={SIZE} height={SIZE} />
       </Menu.Button>
       <Fade>{children}</Fade>
     </Menu>
