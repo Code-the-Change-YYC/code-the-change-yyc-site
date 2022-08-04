03f060b1e3452211315fa7fd11b73c859f9efa82
Harsohail Brar <harsohail.brar1@gmail.com>
2022-08-03 20:25:58 -0700

    mobile navbar drawer

---
+++ components/MobileNavbarDrawer.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+
+const textClasses = "text-4xl font-semibold text-white";
+
+const NestedLinks = ({ options }) => {
+  return (
+    <div className="ml-5 flex flex-col font-normal text-white">
+      {options.map((option) => (
+        <a key={option} href={`/${option.toLowerCase().replace(" ", "-")}`} className="text-xl py-1">
+          / {option}
+        </a>
+      ))}
+    </div>
+  );
+};
+
+const MobileNavbarDrawer = () => {
+  const [selectedLink, setSelectedLink] = useState(null);
+
+  const handleClick = (link) => {
+    if (selectedLink === link) {
+      setSelectedLink(null);
+    } else {
+      setSelectedLink(link);
+    }
+  };
+
+  return (
+    <div className="flex flex-col space-y-2 bg-[#FF4D6F] rounded-b-2xl z-5 -mt-5 p-10 absolute top-full w-full">
+      <h1
+        className={`cursor-pointer ${textClasses} ${selectedLink === "about" ? "underline" : ""}`}
+        onClick={() => handleClick("about")}
+      >
+        About
+      </h1>
+      {selectedLink === "about" && <NestedLinks options={["Who we are", "Projects"]} />}
+      <h1
+        className={`cursor-pointer ${textClasses} ${selectedLink === "news" ? "underline" : ""}`}
+        onClick={() => handleClick("news")}
+      >
+        News
+      </h1>
+      {selectedLink === "news" && <NestedLinks options={["Announcements", "Blog"]} />}
+      <a className={textClasses} href="partnerships">
+        Partnerships
+      </a>
+      <a className={textClasses} href="contact">
+        Contact
+      </a>
+      <a className={textClasses} href="join">
+        Join
+      </a>
+    </div>
+  );
+};
+
+export default MobileNavbarDrawer;
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,59 +1,71 @@
 import { Menu } from "@headlessui/react";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import Dropdown from "./Dropdown";
 import { FaBars } from "react-icons/fa";
+import MobileNavbarDrawer from "./MobileNavbarDrawer";
 
 const textClasses = "text-xl font-semibold text-[#FF4D6F]";
 const optionClasses = "text-xl font-semibold text-white p-2";
 const optionsClasses = "absolute right-0 top-12 w-44 rounded-b-lg bg-[#FF4D6F] p-2";
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
   return (
-    <div className="rounded-b-2xl md:rounded-none flex justify-between items-center px-10 md:px-20 py-5 bg-white drop-shadow-lg">
-      <FaBars className="md:hidden text-[#A689FF] cursor-pointer" />
+    <div className="flex flex-col relative">
+      <div className="rounded-b-2xl md:rounded-none flex justify-between items-center px-10 md:px-20 py-5 bg-white drop-shadow-lg z-10">
+        <FaBars className="md:hidden text-[#A689FF] cursor-pointer" onClick={() => setOpen(!open)} />
 
-      <Image src="/logo.png" alt="logo" width={44} height={44} />
+        <Image src="/logo.png" alt="logo" width={44} height={44} />
 
-      <div className="hidden md:flex md:items-center md:space-x-8">
-        <Dropdown
-          title="About"
-          titleClasses={textClasses}
-          optionsClasses={optionsClasses}
-          optionClasses={optionClasses}
-        >
-          <Menu.Items as="div" className={`absolute right-0 mt-2 w-36 origin-top-right divide-y ${optionsClasses}`}>
-            <Menu.Item className={`flex justify-center items-center ${optionClasses}`}>
-              <a href="/who-we-are">Who we are</a>
-            </Menu.Item>
-            <Menu.Item className={`flex justify-center items-center ${optionClasses}`}>
-              <a href="/projects">Projects</a>
-            </Menu.Item>
-          </Menu.Items>
-        </Dropdown>
-        <Dropdown title="News" titleClasses={textClasses} optionsClasses={optionsClasses} optionClasses={optionClasses}>
-          <Menu.Items as="div" className={`absolute right-0 mt-2 w-36 origin-top-right divide-y ${optionsClasses}`}>
-            <Menu.Item className={`flex justify-center items-center ${optionClasses}`}>
-              <a href="/announcements">Announcements</a>
-            </Menu.Item>
-            <Menu.Item className={`flex justify-center items-center ${optionClasses}`}>
-              <a href="/blog">Blog</a>
-            </Menu.Item>
-          </Menu.Items>
-        </Dropdown>
-        <Link href="/partnerships">
-          <span className={textClasses}>Partnerships</span>
-        </Link>
-        <Link href="/contact">
-          <span className={textClasses}>Contact</span>
-        </Link>
-        <Link href="/join">
-          <span className={textClasses}>Join</span>
-        </Link>
+        <div className="hidden md:flex md:items-center md:space-x-8">
+          <Dropdown
+            title="About"
+            titleClasses={textClasses}
+            optionsClasses={optionsClasses}
+            optionClasses={optionClasses}
+          >
+            <Menu.Items as="div" className={`absolute right-0 mt-2 w-36 origin-top-right divide-y ${optionsClasses}`}>
+              <Menu.Item className={`flex justify-center items-center ${optionClasses}`}>
+                <a href="/who-we-are">Who we are</a>
+              </Menu.Item>
+              <Menu.Item className={`flex justify-center items-center ${optionClasses}`}>
+                <a href="/projects">Projects</a>
+              </Menu.Item>
+            </Menu.Items>
+          </Dropdown>
+          <Dropdown
+            title="News"
+            titleClasses={textClasses}
+            optionsClasses={optionsClasses}
+            optionClasses={optionClasses}
+          >
+            <Menu.Items as="div" className={`absolute right-0 mt-2 w-36 origin-top-right divide-y ${optionsClasses}`}>
+              <Menu.Item className={`flex justify-center items-center ${optionClasses}`}>
+                <a href="/announcements">Announcements</a>
+              </Menu.Item>
+              <Menu.Item className={`flex justify-center items-center ${optionClasses}`}>
+                <a href="/blog">Blog</a>
+              </Menu.Item>
+            </Menu.Items>
+          </Dropdown>
+          <Link href="/partnerships">
+            <span className={textClasses}>Partnerships</span>
+          </Link>
+          <Link href="/contact">
+            <span className={textClasses}>Contact</span>
+          </Link>
+          <Link href="/join">
+            <span className={textClasses}>Join</span>
+          </Link>
+        </div>
+
+        <div className="md:hidden"></div>
       </div>
 
-      <div className="md:hidden"></div>
+      {open && <MobileNavbarDrawer />}
     </div>
   );
 };
