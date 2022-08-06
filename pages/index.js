232d4e16d4bf766c29a5494f7d2f7bc4e1d033fb
Harsohail Brar <harsohail.brar1@gmail.com>
2022-08-06 16:12:50 -0700

    progress

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div className="flex flex-col">
@@ -7,23 +9,28 @@ export default function Home() {
         </h1>
       </div>
 
-      <div className="flex justify-between bg-[#7055FD] md:bg-white py-48 px-10 md:px-48 rounded-2xl drop-shadow-lg md:drop-shadow-none">
-        <div className="flex flex-col md:w-1/3">
+      <div className="relative flex justify-between bg-[#7055FD] md:bg-white py-60 px-10 md:px-48 rounded-2xl drop-shadow-lg md:drop-shadow-none">
+        <img
+          src="/svgs/homepage/with_logo.svg"
+          alt="logo_svg"
+          className="md:hidden absolute top-0 mx-auto left-0 right-0 pt-16 object-fill w-[400px]"
+        />
+        <div className=" flex flex-col md:w-1/3">
           <h2 className="hidden md:block text-5xl text-[#FF4D6F] font-bold">code the change yyc</h2>
           <p className="text-2xl text-white md:text-[#A689FF] font-medium">
-            <strong className="md:hidden">code the change yyc</strong> is a student-led initiative where students in
-            technology-focused programs volunteer their time to work on projects for causes as a way to grow their
-            skills, gain experience, attain mentorship, and give back to the community.
+            <strong className="md:hidden text-3xl">code the change yyc</strong> is a student-led initiative where
+            students in technology-focused programs volunteer their time to work on projects for causes as a way to grow
+            their skills, gain experience, attain mentorship, and give back to the community.
           </p>
         </div>
         {/* TODO: Add image here */}
         <div></div>
       </div>
 
-      <div className="flex justify-between py-48 px-10 md:px-48 items-center divide-x-4 divide-[#A689FF] bg-[#7055FD]">
-        <div className="flex flex-col w-1/2 pr-32">
+      <div className="flex flex-col md:flex-row justify-center md:justify-between py-48 px-10 md:px-48 items-center md:divide-x-4 md:divide-[#A689FF] bg-white md:bg-[#7055FD]">
+        <div className="flex flex-col mt-10 md:w-1/2 md:pr-32 md:mt-0">
           <h1 className="text-5xl font-bold">Coding</h1>
-          <p className="text-2xl text-white font-medium mt-4">
+          <p className="text-2xl text-black md:text-white font-medium mt-4 text-center md:text-left">
             We seek to partner with causes that have projects on the go that need additional programming and software
             development support. We will work with you to find the right tool for the job. We prefer projects that are
             already underway with a good vision, but require a lot more technical work to move it along.
@@ -32,9 +39,9 @@ export default function Home() {
           <div></div>
         </div>
 
-        <div className="flex flex-col w-1/2 pl-32">
+        <div className="flex flex-col md:w-1/2 md:pl-32">
           <h1 className="text-5xl font-bold">Community</h1>
-          <p className="text-2xl text-white font-medium mt-4">
+          <p className="text-2xl text-black md:text-white font-medium mt-4 text-center md:text-left">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Leo nec vitae ac faucibus. Blandit ultrices magna
             natoque enim quis suscipit. Vitae eget venenatis rhoncus aliquet curabitur mauris, sed turpis nulla. Neque
             molestie mi placerat ultrices sit in sit pretium ut.
