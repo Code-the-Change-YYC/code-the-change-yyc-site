be94f0b6b7bc943111650c55abfcd2d42ef00588
Tyler Lam <tylerl264@gmail.com>
2022-08-06 15:55:15 -0700

    fix: Correct styling

--- components/Heading.jsx
+++ components/Heading.jsx
@@ -3,13 +3,11 @@ const HEADING_CONTAINER = "p-5 flex flex-col items-center";
 const LONG_UNDERLINE_DIR = "/svgs/long_heading_underline.svg";
 const SHORT_UNDERLINE_DIR = "/svgs/short_heading_underline.svg";
 
-const Heading = ({ text, longUnderline = false }) => {
-  const renderUnderline = () => <img src={longUnderline ? LONG_UNDERLINE_DIR : SHORT_UNDERLINE_DIR} />;
-
+const Heading = ({ textColor, longUnderline = false, children }) => {
   return (
     <div className={HEADING_CONTAINER}>
-      <h1 className="text-black text-5xl font-semibold">{text}</h1>
-      {renderUnderline()}
+      <h1 className={`text-${textColor} text-5xl font-semibold`}>{children}</h1>
+      <img src={longUnderline ? LONG_UNDERLINE_DIR : SHORT_UNDERLINE_DIR} alt="" />
     </div>
   );
 };
