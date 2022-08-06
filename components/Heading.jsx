7e67c2d7b572205d3c5b7da056787d2c2fb30df6
Tyler Lam <tylerl264@gmail.com>
2022-08-06 00:03:36 -0700

    feat: Created heading component

---
+++ components/Heading.jsx
@@ -0,0 +1,17 @@
+const HEADING_CONTAINER = "p-5";
+
+const LONG_UNDERLINE_DIR = "/svgs/long_heading_underline.svg";
+const SHORT_UNDERLINE_DIR = "/svgs/short_heading_underline.svg";
+
+const Heading = ({ text, longUnderline = false }) => {
+  const renderUnderline = () => <img src={longUnderline ? LONG_UNDERLINE_DIR : SHORT_UNDERLINE_DIR} />;
+
+  return (
+    <div className={HEADING_CONTAINER}>
+      <h1 className="text-black text-5xl font-semibold">{text}</h1>
+      {renderUnderline()}
+    </div>
+  );
+};
+
+export default Heading;
