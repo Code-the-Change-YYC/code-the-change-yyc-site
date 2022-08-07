4b7f33b4ce6487ad7d29ab2e1ebca7346f35bb5a
Harsohail Brar <harsohail.brar1@gmail.com>
2022-08-06 17:03:17 -0700

    getting close

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col">
       <div className="flex h-screen justify-center items-center bg-[url('../public/images/homepage/homepage_background.png')]">
-        <h1 className="text-8xl -mt-20 md:m-0 md:text-9xl font-black w-3/4 md:text-center">
+        <h1 className="text-7xl -mt-20 md:m-0 md:text-9xl font-black w-3/4 md:text-center">
           Coding a better world together.
         </h1>
       </div>
@@ -13,12 +13,12 @@ export default function Home() {
         <img
           src="/svgs/homepage/with_logo.svg"
           alt="logo_svg"
-          className="md:hidden absolute top-0 mx-auto left-0 right-0 pt-16 object-fill w-[400px]"
+          className="md:hidden absolute top-0 mx-auto left-0 right-0 pt-20 object-fill w-[400px]"
         />
         <div className=" flex flex-col md:w-1/3">
           <h2 className="hidden md:block text-5xl text-[#FF4D6F] font-bold">code the change yyc</h2>
-          <p className="text-2xl text-white md:text-[#A689FF] font-medium">
-            <strong className="md:hidden text-3xl">code the change yyc</strong> is a student-led initiative where
+          <p className="text-xl text-white md:text-[#A689FF] font-medium">
+            <strong className="md:hidden text-2xl">code the change yyc</strong> is a student-led initiative where
             students in technology-focused programs volunteer their time to work on projects for causes as a way to grow
             their skills, gain experience, attain mentorship, and give back to the community.
           </p>
@@ -27,8 +27,8 @@ export default function Home() {
         <div></div>
       </div>
 
-      <div className="flex flex-col md:flex-row justify-center md:justify-between py-52 md:px-48 items-center md:divide-x-4 md:divide-[#A689FF] bg-white md:bg-[#7055FD]">
-        <div className="flex flex-col md:w-1/2 md:pr-32 md:mt-0">
+      <div className="flex flex-col md:flex-row justify-center md:justify-between pt-40 md:px-48 items-center md:divide-x-4 md:divide-[#A689FF] bg-white md:bg-[#7055FD]">
+        <div className="flex flex-col md:w-1/2 md:pr-32 md:mt-0 px-5 md:px-0">
           <Heading classes="md:text-white">Coding</Heading>
           <p className="text-2xl text-black md:text-white font-medium mt-4 text-center md:text-left">
             We seek to partner with causes that have projects on the go that need additional programming and software
@@ -39,13 +39,9 @@ export default function Home() {
           <div></div>
         </div>
 
-        <img
-          src="/svgs/homepage/coding_community.svg"
-          alt="logo_svg"
-          className="md:hidden pt-16 object-fill w-[400px]"
-        />
+        <img src="/svgs/homepage/coding_community.svg" alt="logo_svg" className="md:hidden object-fill w-[400px]" />
 
-        <div className="flex flex-col md:w-1/2 md:pl-32">
+        <div className="flex flex-col md:w-1/2 md:pl-32 px-5 md:px-0">
           <Heading longUnderline classes="md:text-white">
             Community
           </Heading>
