b1bdd0968a7b69ecc5ae927df4731a6feaff2b4b
Harsohail Brar <harsohail.brar1@gmail.com>
2022-08-07 15:13:21 -0700

    finish

--- pages/who-we-are/index.js
+++ pages/who-we-are/index.js
@@ -2,17 +2,35 @@ import { Disclosure } from "@headlessui/react";
 import { FaAngleDown } from "react-icons/fa";
 import Heading from "../../components/Heading";
 
+const FAQS = [
+  {
+    prompt: "Do I need technical knowledge to join?",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Leo nec vitae ac faucibus. Blandit ultrices magna natoque enim quis suscipit. Vitae eget venenatis rhoncus aliquet curabitur mauris, sed turpis nulla. Neque molestie mi placerat ultrices sit in sit pretium.",
+  },
+  {
+    prompt: "How much time should I expect to commit per week?",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Leo nec vitae ac faucibus. Blandit ultrices magna natoque enim quis suscipit. Vitae eget venenatis rhoncus aliquet curabitur mauris, sed turpis nulla. Neque molestie mi placerat ultrices sit in sit pretium.",
+  },
+  {
+    prompt: "Question 3",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Leo nec vitae ac faucibus. Blandit ultrices magna natoque enim quis suscipit. Vitae eget venenatis rhoncus aliquet curabitur mauris, sed turpis nulla. Neque molestie mi placerat ultrices sit in sit pretium.",
+  },
+];
+
 const FAQDropdown = ({ prompt, content }) => {
   return (
-    <div className="w-full rounded-2xl bg-white p-2">
+    <div className="w-full rounded-2xl bg-white drop-shadow-md">
       <Disclosure>
         {({ open }) => (
           <>
-            <Disclosure.Button className="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
-              <span>{prompt}</span>
-              <FaAngleDown className={`${open ? "rotate-180 transform" : ""} h-5 w-5 text-purple-500`} />
+            <Disclosure.Button className="items-center drop-shadow-md flex w-full justify-between rounded-lg bg-[#7055FD] px-8 py-4 text-left focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+              <span className="text-white italic text-xl font-medium">"{prompt}"</span>
+              <FaAngleDown className={`${open ? "rotate-180 transform" : ""} h-5 w-5 text-white`} />
             </Disclosure.Button>
-            <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">{content}</Disclosure.Panel>
+            <Disclosure.Panel className="px-8 py-4 text-sm font-medium text-base">{content}</Disclosure.Panel>
           </>
         )}
       </Disclosure>
@@ -25,8 +43,10 @@ const WhoWeAre = () => {
     <div className="flex flex-col w-full">
       <div className="flex flex-col bg-white px-10 md:px-24 lg:px-48 w-full">
         <Heading>FAQ</Heading>
-        <div className="flex flex-col py-10 w-full">
-          <FAQDropdown prompt="test" content="test" />
+        <div className="flex flex-col py-10 w-full space-y-4">
+          {FAQS.map(({ prompt, content }) => (
+            <FAQDropdown prompt={prompt} content={content} />
+          ))}
         </div>
       </div>
     </div>
