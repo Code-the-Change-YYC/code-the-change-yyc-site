aa0fd2263a84b2d5245aa9d92b2820d754b83b8a
Tyler Lam <tylerl264@gmail.com>
2022-08-07 15:38:48 -0700

    fix: Change sizing of sponsor images

--- components/Sponsors.jsx
+++ components/Sponsors.jsx
@@ -2,31 +2,41 @@ import Image from "next/image";
 import Heading from "./Heading";
 
 const SPONSORS_CONTAINER = "bg-white flex flex-col items-center";
-const HEADING_CONTAINER = "flex flex-row justify-center";
+const HEADING_CONTAINER = "flex flex-row justify-center mb-10";
 const SPONSORS_LEVEL_CONTAINER = "flex flex-row justify-center items-center my-5";
-const CONTENT_CONTAINER = "my-10 font-medium max-w-sm text-center";
+const CONTENT_CONTAINER = "my-10 font-medium max-w-md text-center";
 const UNDERLINE_CONTAINER = "mb-10";
+const UNDERLINE_PROPERTIES = {
+  width: 177,
+  height: 10,
+};
 
 const Sponsors = () => {
   const goldSponsorsLevelOne = [
-    { file: "Benevity.png", width: 235, height: 70 },
-    { file: "Hunter Hub.png", width: 350, height: 50 },
+    { file: "Benevity.png", multiplier: 3.216, height: 90 },
+    { file: "Hunter Hub.png", multiplier: 7.02, height: 70 },
   ];
 
   const goldSponsorsLevelTwo = [
-    { file: "Arcurve.png", width: 350, height: 35 },
-    { file: "Cisco Secure.jpg", width: 255, height: 75 },
+    { file: "Arcurve.png", multiplier: 10, height: 50 },
+    { file: "Cisco Secure.jpg", multiplier: 3.41, height: 120 },
   ];
 
   const silverSponsors = [
-    { file: "Schulich School of Engineering.png", width: 116, height: 80 },
-    { file: "AltaML.png", width: 185, height: 65 },
+    { file: "Schulich School of Engineering.png", multiplier: 1.45, height: 115 },
+    { file: "AltaML.png", multiplier: 2.86, height: 100 },
   ];
 
   const bronzeSponsors = [
-    { file: "Helcim.svg", width: 75, height: 75 },
-    { file: "Village Ice Cream.png", width: 75, height: 75 },
+    { file: "Helcim.svg", multiplier: 1, height: 75 },
+    { file: "Village Ice Cream.png", multiplier: 1, height: 75 },
   ];
+
+  const SponsorImage = ({ key, file, multiplier, height }) => (
+    <div key={key} className="mx-5">
+      <Image src={`/sponsor_logos/${file}`} alt={file} width={height * multiplier} height={height} />
+    </div>
+  );
 
   return (
     <div className={SPONSORS_CONTAINER}>
@@ -35,31 +45,23 @@ const Sponsors = () => {
         <Image src="/svgs/Sponsorship Heart.svg" alt="" height="100%" width="100%" />
       </div>
       <div className={SPONSORS_LEVEL_CONTAINER}>
-        {goldSponsorsLevelOne.map(({ file, width, height }) => (
-          <div key={file} className="mx-5">
-            <Image src={`/sponsor_logos/${file}`} alt={file} width={width} height={height} />
-          </div>
+        {goldSponsorsLevelOne.map(({ file, ...props }) => (
+          <SponsorImage key={file} file={file} {...props} />
         ))}
       </div>
       <div className={SPONSORS_LEVEL_CONTAINER}>
-        {goldSponsorsLevelTwo.map(({ file, width, height }) => (
-          <div key={file} className="mx-5">
-            <Image src={`/sponsor_logos/${file}`} alt={file} width={width} height={height} />
-          </div>
+        {goldSponsorsLevelTwo.map(({ file, ...props }) => (
+          <SponsorImage key={file} file={file} {...props} />
         ))}
       </div>
       <div className={SPONSORS_LEVEL_CONTAINER}>
-        {silverSponsors.map(({ file, width, height }) => (
-          <div key={file} className="mx-5">
-            <Image key={file} src={`/sponsor_logos/${file}`} alt={file} width={width} height={height} />
-          </div>
+        {silverSponsors.map(({ file, ...props }) => (
+          <SponsorImage key={file} file={file} {...props} />
         ))}
       </div>
       <div className={SPONSORS_LEVEL_CONTAINER}>
-        {bronzeSponsors.map(({ file, width, height }) => (
-          <div key={file} className="mx-5">
-            <Image key={file} src={`/sponsor_logos/${file}`} alt={file} width={width} height={height} />
-          </div>
+        {bronzeSponsors.map(({ file, ...props }) => (
+          <SponsorImage key={file} file={file} {...props} />
         ))}
       </div>
       <div className={CONTENT_CONTAINER}>
@@ -67,7 +69,12 @@ const Sponsors = () => {
         These are the sponsors that help make our events and ideas possible.
       </div>
       <div className={UNDERLINE_CONTAINER}>
-        <Image src="/svgs/Purple Underline.svg" alt="" width={177} height={10} />
+        <Image
+          src="/svgs/Purple Underline.svg"
+          alt=""
+          width={UNDERLINE_PROPERTIES.width}
+          height={UNDERLINE_PROPERTIES.height}
+        />
       </div>
     </div>
   );
