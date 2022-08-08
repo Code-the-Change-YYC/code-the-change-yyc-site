bed404f9c6571b488fed3fffe2dcf683f2b07dd0
Tyler Lam <tylerlam@fb.com>
2022-08-08 15:54:03 -0700

    fix: Add export

--- data/sponsors.js
+++ data/sponsors.js
@@ -1,20 +1,20 @@
 // Multiplier is the value you need to multiply height by to get width
-const GOLD_SPONSORS_ONE = [
+export const GOLD_SPONSORS_ONE = [
   { file: "Benevity.png", multiplier: 3.216, height: 90, website: "benevity.com" },
   { file: "Hunter Hub.png", multiplier: 7.02, height: 70, website: "ucalgary.ca/hunter-hub" },
 ];
 
-const GOLD_SPONSORS_TWO = [
+export const GOLD_SPONSORS_TWO = [
   { file: "Arcurve.png", multiplier: 10, height: 50, website: "arcurve.com" },
   { file: "Cisco Secure.jpg", multiplier: 3.41, height: 120, website: "cisco.com/site/us/en/products/security" },
 ];
 
-const SILVER_SPONSORS = [
+export const SILVER_SPONSORS = [
   { file: "Schulich School of Engineering.png", multiplier: 1.45, height: 115, website: "schulich.ucalgary.ca" },
   { file: "AltaML.png", multiplier: 2.86, height: 100, website: "altaml.com" },
 ];
 
-const BRONZE_SPONSORS = [
+export const BRONZE_SPONSORS = [
   { file: "Helcim.svg", multiplier: 1, height: 75, website: "helcim.com" },
   { file: "Village Ice Cream.png", multiplier: 1, height: 75, website: "villageicecream.com" },
 ];
