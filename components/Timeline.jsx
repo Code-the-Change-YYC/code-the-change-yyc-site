875620421a4672b18df5887dd914bd260a65072d
Tyler Lam <tylerlam@fb.com>
2022-08-08 17:24:50 -0700

    feat: Draw a line

---
+++ components/Timeline.jsx
@@ -0,0 +1,34 @@
+const TIMELINE_DATA = [
+    {
+        year: 2019,
+        description: "We opened this year!",
+        image: ""
+    },
+    {
+        year: 2020,
+        description: "Another year happened",
+        image: ""
+    },
+    {
+        year: 2021,
+        description: "Topan joined this year :)",
+        image: ""
+    },
+    {
+        year: 2022,
+        description: "This is the current year",
+        image: ""
+    }
+];
+
+const TIMELINE_CONTAINER = "bg-white flex flex-col items-center p-10"
+
+const Timeline = () => {
+    return (
+        <div className={TIMELINE_CONTAINER}>
+            <div className="border-l-4 border-[#7055FD] rounded-lg h-10" />
+        </div>
+    )
+}
+
+export default Timeline;
--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Navbar from "../components/Navbar";
 import "../styles/globals.css";
 import Footer from "../components/Footer";
+import Timeline from "../components/Timeline";
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -13,9 +14,7 @@ function MyApp({ Component, pageProps }) {
       <Navbar />
       <Component {...pageProps} />
       {/* CONTAINER FOR CONTENT BELOW TO TEST NAVBAR IS IN RIGHT POSITION*/}
-      <div className="mb-auto bg-green-500">
-        <h1>content</h1>
-      </div>
+      <Timeline />
       <Footer />
     </div>
   );
