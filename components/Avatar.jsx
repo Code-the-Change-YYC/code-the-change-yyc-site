9550b9758ce972ddcb5ce5554d35b729c0efaec4
Tyler Lam <tylerl264@gmail.com>
2022-08-09 23:03:30 -0700

    fix: Open linkedin in new tab

--- components/Avatar.jsx
+++ components/Avatar.jsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
@@ -6,14 +5,14 @@ const PROFILE_CONTAINER = "flex flex-col items-center m-10";
 
 const Avatar = ({ name, file, position, linkedin }) => {
   const FontAwesomeLink = ({ username, icon }) => (
-    <Link href={`https://linkedin.com/in/${username}`}>
+    <a target="_blank" href={`https://linkedin.com/in/${username}`} rel="noopener noreferrer">
       <FontAwesomeIcon className="h-6 w-6 text-[#7055FD] cursor-pointer" icon={icon} />
-    </Link>
+    </a>
   );
 
   return (
     <div className={PROFILE_CONTAINER}>
-      <img src={`/profiles/${file}`} alt={name} className={`w-28 sm:w-48 rounded-full`}/>
+      <img src={`/profiles/${file}`} alt={name} className={`w-28 sm:w-48 rounded-full`} />
       <h2 className="text-[#7055FD] text-xs sm:text-base font-semibold mt-3">{name}</h2>
       <h2 className="text-[#7055FD] text-xs sm:text-base font-medium mb-2">{position}</h2>
       <FontAwesomeLink username={linkedin} icon={faLinkedin} />
