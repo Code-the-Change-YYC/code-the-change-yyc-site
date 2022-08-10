bedeb7f04d0231eb1f63e611298da912ff606f60
Tyler Lam <tylerlam@fb.com>
2022-08-10 16:31:59 -0700

    feat: Create timeline structure

--- components/Timeline.jsx
+++ components/Timeline.jsx
@@ -21,13 +21,42 @@ const TIMELINE_DATA = [
     }
 ];
 
-const TIMELINE_CONTAINER = "bg-white flex flex-col items-center p-10"
+const TIMELINE_CONTAINER = "bg-white flex flex-col"
 
 const Timeline = () => {
     return (
+        <>
         <div className={TIMELINE_CONTAINER}>
-            <div className="border-l-4 border-[#7055FD] rounded-lg h-24" />
+            <div className="flex flex-row">
+                <div className="flex flex-col w-48">
+                    <div className="box-border h-24 w-24 bg-[#BAFBE4] drop-shadow-md" />
+                    <div className="flex flex-row ml-auto mt-10">
+                        <div className="px-5 text-5xl font-semibold">2019</div>
+                        <div className="top-1/2 relative border-t-4 border-[#7055FD] w-10" />
+                    </div>
+                    <div className="w-40">Code the Change YYC began as an organization in 2019. </div>
+                </div>
+                <div className="border-l-4 border-[#7055FD] h-30" />
+                <div className="box-border h-36 w-36 bg-[#A689FF] ml-8 mt-16 drop-shadow-md" />
+            </div>
         </div>
+        <div className={TIMELINE_CONTAINER}>
+            <div className="flex flex-row">
+                <div className="w-48">
+                    <div className="box-border h-36 w-36 bg-[#A689FF] mr-12 mt-16 drop-shadow-md" />
+                </div>
+                <div className="border-l-4 border-[#7055FD] h-30" />
+                <div className="flex flex-col">
+                    <div className="box-border h-24 w-24 bg-[#BAFBE4] ml-8 drop-shadow-md" />
+                    <div className="flex flex-row ml-auto mt-10">
+                        <div className="top-1/2 relative border-t-4 border-[#7055FD] w-10" />
+                        <div className="px-5 text-5xl font-semibold">2020</div>
+                    </div>
+                    <div className="w-40 ml-8">Code the Change YYC began as an organization in 2019. </div>
+                </div>
+            </div>
+        </div>
+        </>
     )
 }
 
