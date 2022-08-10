6a3c2deb6ee1586c1224eb4f14d48c58fba41404
Tyler Lam <tylerl264@gmail.com>
2022-08-09 23:06:30 -0700

    fix: Increase width

--- components/Executives.jsx
+++ components/Executives.jsx
@@ -3,7 +3,7 @@ import Avatar from "./Avatar";
 import { EXECUTIVES } from "../data/executives";
 
 const EXECUTIVES_CONTAINER = "bg-white flex flex-col items-center mb";
-const EXECUTIVE_LEVEL_CONTAINER = "flex flex-row flex-wrap justify-center w-full sm:w-8/12 mb-5";
+const EXECUTIVE_LEVEL_CONTAINER = "flex flex-row flex-wrap justify-center w-full sm:w-9/12 mb-5";
 
 const Executives = () => {
   return (
