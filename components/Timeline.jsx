57071a84a0bf42d23c02a19148815305f4ba5821
Tyler Lam <tylerl264@gmail.com>
2022-08-15 16:23:39 -0600

    fix: Correct timeline sizes

--- components/Timeline.jsx
+++ components/Timeline.jsx
@@ -29,28 +29,28 @@ const Timeline = () => {
       <div className={TIMELINE_CONTAINER}>
         <div className="flex flex-row flex-start">
           <div className="flex flex-col items-center">
-            <div className="box-border h-32 w-48 bg-[#BAFBE4] mx-8 mt-10 rounded-lg drop-shadow-md" />
+            <div className="box-border h-32 w-36 md:h-32 md:w-48 bg-[#BAFBE4] mx-4 mt-10 rounded-lg drop-shadow-md" />
             <div className="flex flex-row ml-auto mt-10">
-              <div className="px-5 text-5xl font-semibold">2019</div>
+              <div className="px-3 text-5xl font-semibold">2019</div>
               <div className="top-1/2 relative border-t-4 border-[#7055FD] w-10" />
             </div>
             <div className="w-40 ml-auto">Code the Change YYC began as an organization in 2019. </div>
           </div>
           <div className="border-l-4 border-[#7055FD] h-30" />
-          <div className="box-border h-48 w-48 bg-[#A689FF] ml-8 mt-16 rounded-l-lg drop-shadow-md" />
+          <div className="box-border h-32 w-44 md:h-48 md:w-48 bg-[#A689FF] ml-8 mt-16 rounded-l-lg drop-shadow-md" />
         </div>
       </div>
       <div className={TIMELINE_CONTAINER}>
         <div className="flex flex-row flex-start">
           <div className="flex flex-col items-center">
-            <div className="box-border h-32 w-56 bg-[#A689FF] mr-8 mt-16 rounded-r-lg drop-shadow-md" />
+            <div className="box-border h-32 w-36 md:h-32 md:w-52 bg-[#A689FF] mr-8 md:mr-4 mt-16 rounded-r-lg drop-shadow-md" />
           </div>
           <div className="border-l-4 border-[#7055FD] h-30" />
           <div className="flex flex-col items-center">
-            <div className="box-border h-32 w-48 bg-[#BAFBE4] ml-8 mt-10 rounded-lg drop-shadow-md" />
+            <div className="box-border h-32 w-36 bg-[#BAFBE4] ml-8 mt-10 rounded-lg drop-shadow-md" />
             <div className="flex flex-row mr-auto mt-10">
               <div className="top-1/2 relative border-t-4 border-[#7055FD] w-10" />
-              <div className="px-5 text-5xl font-semibold">2020</div>
+              <div className="pl-3 text-5xl font-semibold">2020</div>
             </div>
             <div className="w-40 px-10 mr-auto">We did other stuff in 2020. </div>
           </div>
