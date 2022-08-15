c74a43412c2626e2bdec3adb53ed63dbd305ea53
Tyler Lam <tylerl264@gmail.com>
2022-08-15 11:08:58 -0600

    fix: update widths for timeline

--- components/Timeline.jsx
+++ components/Timeline.jsx
@@ -28,8 +28,8 @@ const Timeline = () => {
     <>
       <div className={TIMELINE_CONTAINER}>
         <div className="flex flex-row flex-start">
-          <div className="flex flex-col w-96 items-center">
-            <div className="box-border h-32 w-3/4 bg-[#BAFBE4] mt-10 rounded-lg drop-shadow-md" />
+          <div className="flex flex-col items-center">
+            <div className="box-border h-32 w-48 bg-[#BAFBE4] mx-8 mt-10 rounded-lg drop-shadow-md" />
             <div className="flex flex-row ml-auto mt-10">
               <div className="px-5 text-5xl font-semibold">2019</div>
               <div className="top-1/2 relative border-t-4 border-[#7055FD] w-10" />
@@ -37,17 +37,17 @@ const Timeline = () => {
             <div className="w-40 ml-auto">Code the Change YYC began as an organization in 2019. </div>
           </div>
           <div className="border-l-4 border-[#7055FD] h-30" />
-          <div className="box-border h-48 w-96 bg-[#A689FF] ml-8 mt-16 rounded-lg drop-shadow-md" />
+          <div className="box-border h-48 w-48 bg-[#A689FF] ml-8 mt-16 rounded-l-lg drop-shadow-md" />
         </div>
       </div>
       <div className={TIMELINE_CONTAINER}>
         <div className="flex flex-row flex-start">
-          <div className="flex flex-col w-96 items-center">
-            <div className="box-border h-32 w-3/4 bg-[#A689FF] mt-16 rounded-lg drop-shadow-md" />
+          <div className="flex flex-col items-center">
+            <div className="box-border h-32 w-56 bg-[#A689FF] mr-8 mt-16 rounded-r-lg drop-shadow-md" />
           </div>
           <div className="border-l-4 border-[#7055FD] h-30" />
-          <div className="flex flex-col w-96 items-center">
-            <div className="box-border h-32 w-3/4 bg-[#BAFBE4] mt-10 rounded-lg drop-shadow-md" />
+          <div className="flex flex-col items-center">
+            <div className="box-border h-32 w-48 bg-[#BAFBE4] ml-8 mt-10 rounded-lg drop-shadow-md" />
             <div className="flex flex-row mr-auto mt-10">
               <div className="top-1/2 relative border-t-4 border-[#7055FD] w-10" />
               <div className="px-5 text-5xl font-semibold">2020</div>
