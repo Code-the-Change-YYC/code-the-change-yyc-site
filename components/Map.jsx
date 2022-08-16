fca878fba9dc72aec1cb1a0b29fcf5ad4fe131a8
Harsohail Brar <harsohail.brar1@gmail.com>
2022-08-15 17:49:14 -0700

    fix

--- components/Map.jsx
+++ components/Map.jsx
@@ -1,22 +1,8 @@
 import { ComposableMap, Geographies, Geography, Marker } from "react-simple-maps";
+import { LOCATIONS } from "../data/locations";
 
 const GEO_URL = "https://raw.githubusercontent.com/deldersveld/topojson/master/continents/north-america.json";
-
-const markers = [
-  {
-    name: "University of Calgary",
-    coordinates: [-114.08529, 51.05011],
-  },
-  {
-    name: "Vancouver (UBC)",
-    coordinates: [-123.1207, 49.2827],
-    offset: 15,
-  },
-  {
-    name: "Stanford",
-    coordinates: [-122.1697, 37.4275],
-  },
-];
+const DEFAULT_MARKER_TEXT_OFFSET = -10;
 
 const Map = () => {
   return (
@@ -33,10 +19,10 @@ const Map = () => {
           geographies.map((geo) => <Geography key={geo.rsmKey} geography={geo} fill="#EAEAEC" stroke="#D6D6DA" />)
         }
       </Geographies>
-      {markers.map(({ name, coordinates, offset }) => (
+      {LOCATIONS.map(({ name, coordinates, offset }) => (
         <Marker key={name} coordinates={coordinates}>
           <circle r={3} fill="#FF4D6F" />
-          <text textAnchor="middle" y={offset || -10}>
+          <text textAnchor="middle" y={offset || DEFAULT_MARKER_TEXT_OFFSET}>
             {name}
           </text>
         </Marker>
