26caa2482d8a76809ab566d07261d6c11e53b7e9
Tyler Lam <tylerl264@gmail.com>
2022-08-15 18:44:48 -0600

    fix: Correct images

--- components/Timeline.jsx
+++ components/Timeline.jsx
@@ -1,67 +1,64 @@
-const TIMELINE_DATA = [
-  {
-    year: 2019,
-    description: "We opened this year!",
-    smallImage: "",
-    largeImage: ""
-  },
-  {
-    year: 2020,
-    description: "Another year happened",
-    smallImage: "",
-    largeImage: ""
-  },
-  {
-    year: 2021,
-    description: "Topan joined this year :)",
-    smallImage: "",
-    largeImage: ""
-  },
-  {
-    year: 2022,
-    description: "This is the current year",
-    smallImage: "",
-    largeImage: ""
-  },
-];
+/* eslint-disable @next/next/no-img-element */
+import { TIMELINE_DATA } from "../data/timeline";
 
 const TIMELINE_CONTAINER = "bg-white flex flex-col w-full md:w-1/2";
 
 const Timeline = () => {
-  return (
-    <>
-      <div className={TIMELINE_CONTAINER}>
-        <div className="flex flex-row flex-start">
-          <div className="flex flex-col items-center">
-            <div className="box-border h-32 w-36 md:h-32 md:w-48 bg-[#BAFBE4] mx-4 mt-10 rounded-lg drop-shadow-md" />
-            <div className="flex flex-row ml-auto mt-10">
-              <div className="px-3 text-5xl font-semibold">2019</div>
-              <div className="top-1/2 relative border-t-4 border-[#7055FD] w-10" />
+  return TIMELINE_DATA.map(({ year, description, smallImage, largeImage }, index) => {
+    if (index % 2 == 0) {
+      return (
+        <div className={TIMELINE_CONTAINER}>
+          <div className="flex flex-row flex-start">
+            <div className="flex flex-col items-center">
+              <img
+                src={`/images/timeline/${smallImage}`}
+                alt=""
+                className="box-border h-32 w-36 md:h-32 md:w-48 bg-[#BAFBE4] mx-4 mt-10 rounded-lg drop-shadow-md"
+              />
+              <div className="flex flex-row ml-auto mt-10">
+                <div className="px-3 text-5xl font-semibold">{year}</div>
+                <div className="top-1/2 relative border-t-4 border-[#7055FD] w-10" />
+              </div>
+              <div className="w-40 ml-auto">{description}</div>
             </div>
-            <div className="w-40 ml-auto">Code the Change YYC began as an organization in 2019. </div>
-          </div>
-          <div className="border-l-4 border-[#7055FD] h-30" />
-          <div className="box-border h-32 w-44 md:h-48 md:w-48 bg-[#A689FF] ml-8 mt-16 rounded-l-lg drop-shadow-md" />
-        </div>
-      </div>
-      <div className={TIMELINE_CONTAINER}>
-        <div className="flex flex-row flex-start">
-          <div className="flex flex-col items-center">
-            <div className="box-border h-32 w-36 md:h-32 md:w-52 bg-[#A689FF] mr-8 md:mr-4 mt-16 rounded-r-lg drop-shadow-md" />
-          </div>
-          <div className="border-l-4 border-[#7055FD] h-30" />
-          <div className="flex flex-col items-center">
-            <div className="box-border h-32 w-36 bg-[#BAFBE4] ml-8 mt-10 rounded-lg drop-shadow-md" />
-            <div className="flex flex-row mr-auto mt-10">
-              <div className="top-1/2 relative border-t-4 border-[#7055FD] w-10" />
-              <div className="pl-3 text-5xl font-semibold">2020</div>
-            </div>
-            <div className="w-40 px-10 mr-auto">We did other stuff in 2020. </div>
+            <div className="border-l-4 border-[#7055FD] h-30" />
+            <img
+              src={`/images/timeline/${largeImage}`}
+              alt=""
+              className="box-border h-32 w-44 md:h-32 md:w-52 bg-[#A689FF] ml-8 mt-16 rounded-l-lg drop-shadow-md"
+            />
           </div>
         </div>
-      </div>
-    </>
-  );
+      );
+    } else {
+      return (
+        <div className={TIMELINE_CONTAINER}>
+          <div className="flex flex-row flex-start">
+            <div className="flex flex-col items-center">
+              <img
+                src={`/images/timeline/${smallImage}`}
+                alt=""
+                className="box-border h-32 w-36 md:h-32 md:w-52 bg-[#A689FF] mr-8 md:mr-4 mt-16 rounded-r-lg drop-shadow-md"
+              />
+            </div>
+            <div className="border-l-4 border-[#7055FD] h-30" />
+            <div className="flex flex-col items-center">
+              <img
+                src={`/images/timeline/${largeImage}`}
+                alt=""
+                className="box-border h-32 w-36 md:w-48 bg-[#BAFBE4] ml-8 mt-10 rounded-lg drop-shadow-md"
+              />
+              <div className="flex flex-row mr-auto mt-10">
+                <div className="top-1/2 relative border-t-4 border-[#7055FD] w-10" />
+                <div className="pl-3 text-5xl font-semibold">{year}</div>
+              </div>
+              <div className="w-40 px-10 mr-auto">{description}</div>
+            </div>
+          </div>
+        </div>
+      );
+    }
+  });
 };
 
 export default Timeline;
---
+++ data/timeline.js
@@ -0,0 +1,26 @@
+export const TIMELINE_DATA = [
+  {
+    year: 2019,
+    description: "Code the Change YYC launched this year!",
+    smallImage: "2019_hackathon_image.jpg",
+    largeImage: "2019_hackathon_image_2.jpg",
+  },
+  {
+    year: 2020,
+    description: "Things went virtual this year.",
+    smallImage: "2019_hackathon_image.jpg",
+    largeImage: "2019_hackathon_image_2.jpg",
+  },
+  {
+    year: 2021,
+    description: "Things continued to be virtual this year.",
+    smallImage: "2019_hackathon_image.jpg",
+    largeImage: "2019_hackathon_image_2.jpg",
+  },
+  {
+    year: 2022,
+    description: "This is the current year.",
+    smallImage: "2019_hackathon_image.jpg",
+    largeImage: "2019_hackathon_image_2.jpg",
+  },
+];
