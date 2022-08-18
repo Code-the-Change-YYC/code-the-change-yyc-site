98456bc3607fb870fbd6c29e8b5847670ae19793
Tyler Lam <tylerl264@gmail.com>
2022-08-17 22:52:26 -0600

    feat: Add white flower and upload clearer pic

--- components/About.jsx
+++ components/About.jsx
@@ -2,8 +2,9 @@
 import Heading from "./Heading";
 import TextSection from "./TextSection";
 
-const WINS_VOLUNTEERING_PNG = "/images/about/wins_2022_volunteering.png";
+const WINS_VOLUNTEERING_PNG = "/images/about/wins_2022_volunteering.jpeg";
 const WAVING_HAND_SVG = "/svgs/about/waving_hand.svg";
+const WHITE_FLOWER_SVG = "/svgs/about/white_flower.svg";
 const BLUE_BOLD_TEXT = "text-2xl text-[#BAFBE4]";
 
 const About = () => {
@@ -17,13 +18,14 @@ const About = () => {
           </Heading>
           <img src={WAVING_HAND_SVG} alt="" />
         </div>
-        <TextSection classes="text-white p-14">
+        <TextSection classes="text-white w-2/3 sm:w-1/2 pt-10 sm:pb-20">
           <strong className={BLUE_BOLD_TEXT}>code the change yyc</strong> is a student-led initiative where students in
           technology-focused programs volunteer their time to work on projects for causes as a way to grow their
           <strong className={BLUE_BOLD_TEXT}> skills</strong>, gain
           <strong className={BLUE_BOLD_TEXT}> experience</strong>, attain mentorship, and
           <strong className={BLUE_BOLD_TEXT}> give back</strong> to the community.
         </TextSection>
+        <img src={WHITE_FLOWER_SVG} alt="" className="md:hidden p-10" />
       </div>
     </div>
   );
