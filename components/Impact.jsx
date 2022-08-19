ecf98591680ec6ac0aa0461b2d50363e8f272918
Tyler Lam <tylerl264@gmail.com>
2022-08-18 22:21:57 -0600

    fix: Create mobile view

--- components/Impact.jsx
+++ components/Impact.jsx
@@ -1,14 +1,17 @@
 /* eslint-disable @next/next/no-img-element */
-const HEADER_CONTAINER = "bg-[#7055FD] text-white font-semibold text-4xl p-10 py-15";
-const IMPACT_CONTAINER = "bg-white flex flex-col w-full my-10";
+const HEADER_CONTAINER = "bg-[#7055FD] text-white font-semibold text-4xl p-10 py-15 hidden md:flex";
+const IMPACT_CONTAINER = "bg-white w-full my-10 hidden md:flex";
+const IMPACT_MOBILE_CONTAINER = "bg-white w-full my-5 md:hidden";
 
 const IMPACT_SVG = "/svgs/impact/impact.svg";
+const IMPACT_MOBILE_SVG = "/svgs/impact/impact_mobile.svg";
 
 const Impact = () => {
   return (
     <>
       <div className={HEADER_CONTAINER}>Over the course of our efforts, we have...</div>
       <img src={IMPACT_SVG} alt="" className={IMPACT_CONTAINER} />
+      <img src={IMPACT_MOBILE_SVG} alt="" className={IMPACT_MOBILE_CONTAINER} />
     </>
   );
 };
