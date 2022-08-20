3c3d8ad7456b46b9a11be99439e9949e91212769
Harsohail Brar <harsohail.brar1@gmail.com>
2022-08-20 14:29:58 -0700

    more border rounding

--- components/ProjectTile.jsx
+++ components/ProjectTile.jsx
@@ -6,7 +6,7 @@ const ICON_SIZE = 30;
 /* eslint-disable @next/next/no-img-element */
 const ProjectTile = ({ logo, name, liveProjectLink, githubLink, descriptions }) => {
   return (
-    <div className="flex flex-col items-center bg-white rounded-lg border-4 border-[#A689FF] p-10 m-10 space-y-4">
+    <div className="flex flex-col items-center bg-white rounded-3xl border-4 border-[#A689FF] p-10 m-10 space-y-4">
       <img alt={name} src={`/images/projects/${logo}`} width={200} />
       <h3 className="font-medium text-xl">{name}</h3>
       <div className="flex space-x-4 italic text-[#7055FD]">
