62d16f2faaa8daaa7032a58a23ffedb943be81d9
Harsohail Brar <harsohail.brar1@gmail.com>
2022-08-20 14:25:02 -0700

    add projects

--- components/ProjectTile.jsx
+++ components/ProjectTile.jsx
@@ -6,18 +6,22 @@ const ICON_SIZE = 30;
 /* eslint-disable @next/next/no-img-element */
 const ProjectTile = ({ logo, name, liveProjectLink, githubLink, descriptions }) => {
   return (
-    <div className="flex flex-col items-center bg-white rounded-lg border-4 border-[#A689FF] p-10 space-y-2">
+    <div className="flex flex-col items-center bg-white rounded-lg border-4 border-[#A689FF] p-10 m-10 space-y-2">
       <img alt={name} src={`/images/projects/${logo}`} width={200} />
       <h3 className="font-medium text-xl">{name}</h3>
       <div className="flex space-x-4 italic text-[#7055FD]">
-        <Link href={liveProjectLink}>
-          <FaExternalLinkSquareAlt size={ICON_SIZE} className="cursor-pointer" />
-        </Link>
-        <Link href={githubLink}>
-          <FaGithub size={ICON_SIZE} className="cursor-pointer" />
-        </Link>
+        {liveProjectLink && (
+          <Link href={liveProjectLink}>
+            <FaExternalLinkSquareAlt size={ICON_SIZE} className="cursor-pointer" />
+          </Link>
+        )}
+        {githubLink && (
+          <Link href={githubLink}>
+            <FaGithub size={ICON_SIZE} className="cursor-pointer" />
+          </Link>
+        )}
       </div>
-      <ul className="list-disc text-lg">
+      <ul className="list-disc text-lg w-full">
         {descriptions.map((description) => (
           <li key={description}>{description}</li>
         ))}
--- data/projects.js
+++ data/projects.js
@@ -8,4 +8,22 @@ export const projects = [
       "Built an SMS Twilio integration within a volunteer management system for the nonprofit CASA, which serves foster youth in counties across America",
     ],
   },
+  {
+    logo: "ywca.png",
+    name: "E-learning Storefront",
+    descriptions: [
+      "Creation and design of a storefront website with e-commerce integrated",
+      "Build APIs to interface with companies' different E-Learning platforms",
+      "Work with secure registration and authentication technologies",
+    ],
+  },
+  {
+    logo: "mindfuel.png",
+    name: "Mindfuel System Redesign",
+    descriptions: [
+      "Build an activity board that contains new aggregated data everyday",
+      "Start from scratch or build on top of current system",
+      "Work with skilled developers from the Mindfuel team",
+    ],
+  },
 ];
