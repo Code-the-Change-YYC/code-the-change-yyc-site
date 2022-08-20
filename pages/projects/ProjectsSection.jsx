7e80abac11ab679e30a05d289ce50f24aa82e9f0
Harsohail Brar <harsohail.brar1@gmail.com>
2022-08-20 14:28:02 -0700

    responsive

--- pages/projects/ProjectsSection.jsx
+++ pages/projects/ProjectsSection.jsx
@@ -4,9 +4,9 @@ import { projects } from "../../data/projects";
 
 const ProjectsSection = () => {
   return (
-    <div className="flex flex-col bg-[#7055FD] px-10 md:px-24 lg:px-48 w-full py-20">
+    <div className="flex flex-col bg-[#7055FD] px-2 md:px-24 lg:px-48 w-full py-20">
       <Heading classes="text-white">Our Projects</Heading>
-      <div className="grid grid-cols-2 py-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 py-4 md:py-8">
         {projects.map((project) => (
           <ProjectTile key={project.name} {...project} />
         ))}
