9cb4daafb5a1115379c6739a8816941e5be4e556
Tyler Lam <tylerl264@gmail.com>
2022-08-21 17:41:54 -0600

    fix: Remove unused code block

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,6 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    screens: {
-      xsm: "475px",
-      ...defaultTheme.screens,
-    },
     extend: {
       fontFamily: {
         omnes: ["Omnes", ...defaultTheme.fontFamily.sans],
