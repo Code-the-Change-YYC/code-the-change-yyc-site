622434fecfd40bc4ca668c178ae1a2e468122688
Tyler Lam <tylerl264@gmail.com>
2022-08-21 13:56:18 -0600

    fix: Try new breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,17 +4,20 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      xsm: "475px",
+      ...defaultTheme.screens,
+    },
     extend: {
       fontFamily: {
         omnes: ["Omnes", ...defaultTheme.fontFamily.sans],
       },
       width: {
-        "88": "22rem",
-      }
-    },
-    screens: {
-      xsm: "475px",
-      ...defaultTheme.screens,
+        88: "22rem",
+      },
+      screens: {
+        timelinelg: "1065px",
+      },
     },
   },
   plugins: [require("tailwind-scrollbar-hide")],
