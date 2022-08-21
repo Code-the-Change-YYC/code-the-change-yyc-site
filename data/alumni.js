f9d862af0d260aff14550d5b375023327895aa25
topanb11 <topanbudiman11@gmail.com>
2022-08-20 21:20:56 -0600

    Added alumni

--- data/alumni.js
+++ data/alumni.js
@@ -4,5 +4,20 @@ export const ALUMNI = [
     file: "Jofred Cayabyab.jpg",
     position: "Software Engineer - Meta",
     prevRole: "VP Events",
+		testimonial: "Great opportunity for up and coming students in tech to get involved."
+  },
+  {
+    name: "Avneet Gill",
+    file: "Avneet Gill.jpg",
+    position: "Software Engineer - Amazon",
+    prevRole: "VP Tech",
+		testimonial: "Great opportunity for up and coming students in tech to get involved."
+  },
+  {
+    name: "Oscar Wong",
+    file: "Oscar Wong.jpg",
+    position: "Software Engineer - Figma",
+    prevRole: "VP Tech",
+		testimonial: "Great opportunity for up and coming students in tech to get involved."
   },
 ];
