cfd7dcd4b46cfae9f5b5b4807fb374b08b7d8f7a
Harsohail Brar <47438886+harsohailB@users.noreply.github.com>
2022-08-21 17:29:40 -0700

    Create Projects Section (#41)

---
+++ components/ProjectTile.jsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import { FaExternalLinkSquareAlt, FaGithub } from "react-icons/fa";
+
+const ICON_SIZE = 30;
+
+/* eslint-disable @next/next/no-img-element */
+const ProjectTile = ({ logo, name, liveProjectLink, githubLink, descriptions }) => {
+  return (
+    <div className="flex flex-col items-center bg-white rounded-3xl border-4 border-[#A689FF] p-10 m-10 space-y-4 drop-shadow-md">
+      <img alt={name} src={`/images/projects/${logo}`} width={200} />
+      <h3 className="font-medium text-xl">{name}</h3>
+      <div className="flex space-x-4 italic text-[#7055FD]">
+        {liveProjectLink && (
+          <Link href={liveProjectLink}>
+            <FaExternalLinkSquareAlt size={ICON_SIZE} className="cursor-pointer" />
+          </Link>
+        )}
+        {githubLink && (
+          <Link href={githubLink}>
+            <FaGithub size={ICON_SIZE} className="cursor-pointer" />
+          </Link>
+        )}
+      </div>
+      <ul className="list-disc text-lg w-full">
+        {descriptions.map((description) => (
+          <li key={description}>{description}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ProjectTile;
---
+++ pages/projects/ProjectsSection.jsx
@@ -0,0 +1,18 @@
+import Heading from "../../components/Heading";
+import ProjectTile from "../../components/ProjectTile";
+import { projects } from "../../data/projects";
+
+const ProjectsSection = () => {
+  return (
+    <div className="flex flex-col bg-[#7055FD] px-2 md:px-18 lg:px-36 w-full py-20">
+      <Heading classes="text-white">Our Projects</Heading>
+      <div className="grid grid-cols-1 md:grid-cols-2 py-4 md:py-8">
+        {projects.map((project) => (
+          <ProjectTile key={project.name} {...project} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProjectsSection;
