e4aab46b118192b8a85e9b2e3b58b903fbae4090
Tyler Lam <tylerl264@gmail.com>
2022-08-21 19:42:07 -0600

    fix: Minor tweaks

--- components/AlumniCarousel.jsx
+++ components/AlumniCarousel.jsx
@@ -5,23 +5,25 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/effect-cards";
 
-const CAROUSEL_CONTAINER = "text-white rounded-lg w-[300px] h-[600px] shadow-2xl md:w-[80%]";
+const CAROUSEL_CONTAINER = "text-white rounded-lg w-[20rem] h-[37.5rem] shadow-2xl md:w-4/5";
 const ALUMNI_TILE =
-  "bg-[#7055FD] w-[300px] h-[600px] rounded-lg pt-5 px-5 items-center flex flex-col shadow-2xl md:w-[100%] md:flex-row";
+  "bg-[#7055FD] w-[20rem] h-[37.5rem] rounded-lg items-center p-5 flex flex-col shadow-2xl md:w-full md:flex-row";
 const ALUMNI_PROFILE = "flex flex-col items-center mx-auto";
 
-const AlumniTile = ({ alumni }) => {
+const SPACE_BETWEEN_ELEMENTS = 50;
+
+const AlumniTile = ({ name, file, position, prevRole, testimonial }) => {
   return (
     <div className={ALUMNI_TILE}>
       <div className={ALUMNI_PROFILE}>
-        <img src={`/profiles/${alumni.file}`} alt="" className="w-50 mb-3 mt-5 rounded-full md:w-64 mx-auto" />
-        <h2 className="text-[2rem] font-bold">{alumni.name}</h2>
-        <h2 className="text-[1.25rem] font-semibold text-[#BAFBE4]">{alumni.position}</h2>
-        <h2 className="text-[1.25rem] font-medium mb-5">Previously: {alumni.prevRole}</h2>
+        <img src={`/profiles/${file}`} alt="Profile Picture" className="w-50 mb-3 mt-5 rounded-full md:w-64 mx-auto" />
+        <h2 className="text-[2rem] font-bold">{name}</h2>
+        <h2 className="text-[1.25rem] font-semibold text-[#BAFBE4]">{position}</h2>
+        <h2 className="text-[1.25rem] font-medium mb-5">Previously: {prevRole}</h2>
       </div>
       <p className="text-[1.3rem] font-light italic leading-7 mx-auto md:mb-24 md:text-[1.5rem]">
         <span className="text-[2rem] font-extrabold">&quot;</span>
-        {alumni.testimonial}
+        {testimonial}
         <span className="text-[2rem] font-extrabold">&quot;</span>
       </p>
     </div>
@@ -30,17 +32,15 @@ const AlumniTile = ({ alumni }) => {
 
 const AlumniCarousel = () => {
   return (
-    <>
-      <div className={CAROUSEL_CONTAINER}>
-        <Swiper modules={[Navigation]} spaceBetween={50}>
-          {ALUMNI.map((alumni) => (
-            <SwiperSlide key={alumni.name}>
-              <AlumniTile alumni={alumni} />
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
-    </>
+    <div className={CAROUSEL_CONTAINER}>
+      <Swiper modules={[Navigation]} spaceBetween={SPACE_BETWEEN_ELEMENTS}>
+        {ALUMNI.map((alumni) => (
+          <SwiperSlide key={alumni.name}>
+            <AlumniTile {...alumni} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
   );
 };
 
--- components/AlumniHighlights.jsx
+++ components/AlumniHighlights.jsx
@@ -1,26 +1,25 @@
 /* eslint-disable @next/next/no-img-element */
 import Heading from "./Heading";
 import AlumniCarousel from "./AlumniCarousel";
+import { UnderlineTypes } from "../utils/underlineType";
 
-const ALUMNI_CONTAINER = "flex flex-col items-center h-[1000px] bg-[#FF6B54] pt-3 mb-10 shadow-xl z-0";
-const ALUMNI_HEADER = "pb-10 w-[80%] flex items-left";
+const ALUMNI_CONTAINER = "flex flex-col items-center h-[62.5rem] bg-[#FF6B54] pt-10 shadow-xl z-0";
+const ALUMNI_HEADER = "pb-10 w-4/5 flex items-left";
 const SPARKLE_IMG = "ml-5 mb-5 w-16";
 
 const AlumniHighlights = () => {
   return (
-    <>
-      <div className={ALUMNI_CONTAINER}>
-        <div className={ALUMNI_HEADER}>
-          <Heading classes="text-white" underlineType={false}>
-            <span className="flex flex-row mb-5 md:items-end">
-              Alumni Highlights
-              <img className={SPARKLE_IMG} alt="" src="/svgs/white_sparkle.svg" />
-            </span>
-          </Heading>
-        </div>
-        <AlumniCarousel />
+    <div className={ALUMNI_CONTAINER}>
+      <div className={ALUMNI_HEADER}>
+        <Heading classes="text-white" underlineType={UnderlineTypes.WHITE_LONG_UNDERLINE}>
+          <span className="flex flex-row mb-5 md:items-end">
+            Alumni Highlights
+            <img className={SPARKLE_IMG} alt="" src="/svgs/white_sparkle.svg" />
+          </span>
+        </Heading>
       </div>
-    </>
+      <AlumniCarousel />
+    </div>
   );
 };
 
