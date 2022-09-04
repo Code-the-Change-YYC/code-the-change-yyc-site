ac329385d50bc96e4ed352f573755530891b0ab9
7riumph <triumpharamunde@example.com>
2022-09-04 12:41:39 -0600

    Fix caps and remove drop shadow

--- pages/404.js
+++ pages/404.js
@@ -1,13 +1,13 @@
 import Button from "../components/Button";
 import TextSection from "../components/TextSection.jsx";
 
-const errorPage404 = () => {
+const ErrorPage404 = () => {
   return (
     <div className="py-[10rem] pb-[17rem] md:pb-[25rem] text-center">
       <h1 className="text-9xl font-bold md:text-center text-[#7055FD] py-30">404</h1>
       <TextSection classes="text-[#a689ff] font-light pb-10 py-5">Page not found :(</TextSection>
       <center>
-        <Button href="/" classes="max-w-[19rem] min-w-[19rem] p-[0.6rem] lg:p-[0.7rem] border-[#7055fd] drop-shadow-lg">
+        <Button href="/" classes="max-w-[19rem] min-w-[19rem] p-[0.6rem] lg:p-[0.7rem] border-[#7055fd]">
           <p className="text-[1.5rem] font-[500] text-white">Return to home</p>
         </Button>
       </center>
@@ -15,4 +15,4 @@ const errorPage404 = () => {
   );
 };
 
-export default errorPage404;
+export default ErrorPage404;
