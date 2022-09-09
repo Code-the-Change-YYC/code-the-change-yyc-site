2ba0a921939f2301b84d66c216499d539d101aeb
Harsohail Brar <47438886+harsohailB@users.noreply.github.com>
2022-09-09 11:46:58 -0600

    Add info to main page (#59)
    * add info to main page
    
    * fix layout

--- pages/index.js
+++ pages/index.js
@@ -16,6 +16,7 @@ const LOGO_SVG_BOTTOM = "/svgs/homepage/with_logo_bottom.svg";
 const COMMUNITY_SVG = "/svgs/homepage/coding_community.svg";
 const ROLE_TILE_SIZE = 300;
 const ROLES = ["Development", "Marketing", "Design"];
+const STANDOUT_TEXT_CLASSES = "font-bold text-[#7055FD]";
 
 const RoleTile = ({ role }) => {
   return (
@@ -61,7 +62,7 @@ export default function Home() {
         </h1>
       </div>
 
-      <div className="relative flex -mt-5 justify-between bg-[#7055FD] md:bg-white py-52 px-10 md:px-24 lg:px-48 rounded-2xl drop-shadow-lg md:drop-shadow-none">
+      <div className="relative flex -mt-5 justify-between bg-[#7055FD] py-52 px-10 md:px-24 lg:px-48 rounded-2xl drop-shadow-lg md:drop-shadow-none">
         <img
           src={LOGO_SVG_TOP}
           alt="logo_svg"
@@ -72,47 +73,37 @@ export default function Home() {
           alt="logo_svg"
           className="md:hidden absolute -bottom-36 mx-auto left-0 right-0 pt-20 object-fill w-[400px]"
         />
-        <div className=" flex flex-col md:w-1/3">
-          <h2 className="hidden md:block text-5xl text-[#FF4D6F] font-bold">code the change yyc</h2>
-          <TextSection classes="md:text-[#A689FF]">
-            <strong className="md:hidden text-2xl">code the change yyc</strong> is a student-led initiative where
-            students in technology-focused programs volunteer their time to work on projects for causes as a way to grow
-            their skills, gain experience, attain mentorship, and give back to the community.
-          </TextSection>
-        </div>
-        {/* TODO: Add image here */}
-        <div></div>
+        <TextSection classes="text-white">
+          <strong className="text-4xl">code the change yyc</strong> is a Calgary-based initiative where students with a
+          passion for technology volunteer their time to work on projects for non-profit causes as a way to grow their
+          skills, gain experience, attain mentorship, and give back to the community.
+        </TextSection>
       </div>
 
-      <div className="flex flex-col md:flex-row justify-center md:justify-between pt-40 md:py-40 md:px-24 lg:px-48 items-center md:divide-x-4 md:divide-[#A689FF] bg-white md:bg-[#7055FD]">
+      <div className="flex flex-col md:flex-row justify-center md:justify-between pt-40 md:py-40 md:px-24 lg:px-48 md:divide-x-4 md:divide-[#A689FF] bg-white">
         <div className="flex flex-col md:w-1/2 md:pr-32 md:mt-0 px-5 md:px-0 items-center">
-          <Heading classes="md:text-white">Coding</Heading>
-          <TextSection classes="md:text-white">
-            We seek to partner with causes that have projects on the go that need additional programming and software
-            development support. We will work with you to find the right tool for the job. We prefer projects that are
-            already underway with a good vision, but require a lot more technical work to move it along.
+          <Heading>Coding</Heading>
+          <TextSection>
+            We seek to partner with <span className={STANDOUT_TEXT_CLASSES}>nonprofit impact causes</span> that have
+            projects on the go that need{" "}
+            <span className={STANDOUT_TEXT_CLASSES}>additional programming and software development support</span>. We
+            work with project managers, product designers, and analysts to turn project visions to quality software.
           </TextSection>
-          {/* TODO: Add image here */}
-          <div></div>
         </div>
 
         <img src={COMMUNITY_SVG} alt={COMMUNITY_SVG} className="md:hidden object-fill w-[400px]" />
 
-        <div className="flex flex-col md:w-1/2 md:pl-32 px-5 md:px-0 items-center pt-10 md:pt-0">
-          <Heading underlineType={UnderlineTypes.GREEN_LONG_UNDERLINE} classes="md:text-white">
-            Community
-          </Heading>
-          <TextSection classes="md:text-white">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Leo nec vitae ac faucibus. Blandit ultrices magna
-            natoque enim quis suscipit. Vitae eget venenatis rhoncus aliquet curabitur mauris, sed turpis nulla. Neque
-            molestie mi placerat ultrices sit in sit pretium ut.
+        <div className="flex flex-col md:w-1/2 md:pl-32 px-5 md:px-0 items-center py-10 md:py-0">
+          <Heading underlineType={UnderlineTypes.GREEN_LONG_UNDERLINE}>Community</Heading>
+          <TextSection>
+            Whether it’s through{" "}
+            <span className={STANDOUT_TEXT_CLASSES}>code, workshops, donations, or volunteering</span>, we’re always
+            looking to do something for our local communities! Every year, we donate to charities across Calgary and
+            around the world. We also dedicate time volunteering at local organizations and host technical workshops
+            with a hint of social impact ideologies!
           </TextSection>
-          {/* TODO: Add image here */}
-          <div></div>
         </div>
       </div>
-
-      <Sponsors />
 
       <div className="px-10 md:px-24 lg:px-48 py-20 bg-[#FF6B54]">
         <h1 className="text-5xl font-semibold text-white">Ready to join the change?</h1>
@@ -131,6 +122,8 @@ export default function Home() {
           <Button href="/join">Join Us!</Button>
         </div>
       </div>
+
+      <Sponsors />
     </div>
   );
 }
