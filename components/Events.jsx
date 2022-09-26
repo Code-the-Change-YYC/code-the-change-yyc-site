8d1d38bad8b944968284f0296498a68321c16051
Ajay Arumugam <ajayarumugam07@gmail.com>
2022-09-25 22:43:57 -0600

    added images folder

--- components/Events.jsx
+++ components/Events.jsx
@@ -2,13 +2,35 @@ import React from "react";
 import Heading from "./Heading";
 import { UnderlineTypes } from "../utils/underlineType";
 import TextSection from "./TextSection";
+import Carousel from "./Carousel";
+import { EVENTS_IMAGES } from "../data/events";
+import { Navigation } from "swiper";
+import { SwiperSlide } from "swiper/react";
+import Image from "next/image";
 
 const EVENTS_CONTAINER = "flex flex-col items-center h-[65rem] bg-[#BAFBE4] pt-10 shadow-xl z-0";
 const CONTENT_CONTAINER = "flex flex-col pt-14 p-5 md:px-32";
 const EVENTS_HEADER = "flex flex-row";
+const SPACE_BETWEEN_ELEMENTS = 50;
 
 const Events = () => {
-  const HeaderContent = <span className="flex flex-row mb-2 md:items-end">Events</span>;
+  const EventsCarousel = (
+    <Carousel
+      style={{
+        "--swiper-navigation-color": "#FFF",
+        "--swiper-navigation-size": "25px",
+      }}
+      modules={[Navigation]}
+      navigation
+      spaceBetween={SPACE_BETWEEN_ELEMENTS}
+    >
+      {EVENTS_IMAGES.map((image) => (
+        <SwiperSlide key={image.key}>
+          <Image src={image.file} alt={image.key} layout="fill" objectFit="contain" placeholder="blur" />
+        </SwiperSlide>
+      ))}
+    </Carousel>
+  );
 
   return (
     <div className={EVENTS_CONTAINER}>
@@ -21,6 +43,9 @@ const Events = () => {
           around technology for social impact? We host a variety of workshops and hackathons every year, blending
           together ideas around software development and social good.
         </TextSection>
+        <div className="scrollbar-hide overflow-x-auto">
+          <EventsCarousel />
+        </div>
       </div>
     </div>
   );
---
+++ data/events.js
@@ -0,0 +1,25 @@
+import img1 from "../public/images/events/img1.jpg";
+import img2 from "../public/images/events/img2.jpg";
+import img3 from "../public/images/events/img3.jpg";
+import img4 from "../public/images/events/img4.jpg";
+
+
+
+export const EVENTS_IMAGES = [
+    {
+        file: img1,
+        key: "img1",
+    },
+    {
+        file: img2,
+        key: "img2",
+    },
+    {
+        file: img3,
+        key: "img3",
+    },
+    {
+        file: img4,
+        key: "img4",
+    },
+];
