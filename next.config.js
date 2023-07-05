883224b3da9f574b607c5e7b877aeb76b3b5c539
topanb11 <topanbudiman11@gmail.com>
2023-07-05 17:42:54 -0400

    update gitignore

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  PUBLIC_ACCESS_TOKEN: process.env.NEXT_PUBLIC_ACCESS_TOKEN,
 };
 
 module.exports = nextConfig;
