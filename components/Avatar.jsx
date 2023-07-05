f2817983d88837a73e9a21c8edad680175ae5cf3
topanb11 <topanbudiman11@gmail.com>
2023-07-04 23:34:34 -0400

    fix formatting

--- components/Avatar.jsx
+++ components/Avatar.jsx
@@ -5,22 +5,20 @@ import FontAwesomeLink from './FontAwesomeLink';
 const PROFILE_CONTAINER = 'flex flex-col items-center m-10';
 
 const Avatar = ({ name, linkedin, position, pfp, classes, prevRole }) => {
-    const img = 'https:' + pfp.fields.file.url;
-    return (
-        <div className={PROFILE_CONTAINER}>
-            <div className="w-28 h-28 sm:w-48 sm:h-48 rounded-full relative overflow-hidden">
-                <Image src={img} alt={name} layout="fill" objectFit="contain" unoptimized={true} />
-            </div>
-            <h2 className={`text-[#7055FD] text-xs sm:text-base font-semibold mt-3 ${classes}`}>{name}</h2>
-            <h2 className={`text-[#7055FD] text-xs sm:text-base font-medium mb-2 ${classes}`}>{position}</h2>
-            {prevRole && (
-                <h2 className={`text-[#7055FD] text-xs sm:text-base font-medium mb-2 ${classes}`}>
-                    Previously: {prevRole}
-                </h2>
-            )}
-            {linkedin && <FontAwesomeLink username={linkedin} icon={faLinkedin} />}
-        </div>
-    );
+  const img = 'https:' + pfp.fields.file.url;
+  return (
+    <div className={PROFILE_CONTAINER}>
+      <div className="w-28 h-28 sm:w-48 sm:h-48 rounded-full relative overflow-hidden">
+        <Image src={img} alt={name} layout="fill" objectFit="contain" unoptimized={true} />
+      </div>
+      <h2 className={`text-[#7055FD] text-xs sm:text-base font-semibold mt-3 ${classes}`}>{name}</h2>
+      <h2 className={`text-[#7055FD] text-xs sm:text-base font-medium mb-2 ${classes}`}>{position}</h2>
+      {prevRole && (
+        <h2 className={`text-[#7055FD] text-xs sm:text-base font-medium mb-2 ${classes}`}>Previously: {prevRole}</h2>
+      )}
+      {linkedin && <FontAwesomeLink username={linkedin} icon={faLinkedin} />}
+    </div>
+  );
 };
 
 export default Avatar;
--- components/Executives.jsx
+++ components/Executives.jsx
@@ -8,22 +8,22 @@ const EXECUTIVES_CONTAINER = 'bg-white flex flex-col items-center pt-5';
 const EXECUTIVE_LEVEL_CONTAINER = 'flex flex-row flex-wrap justify-center w-full sm:w-8/12 mb-5';
 
 const Executives = () => {
-    const [executives, setExecutives] = useState([]);
+  const [executives, setExecutives] = useState([]);
 
-    useEffect(() => {
-        fetchContent('adminTeamMember').then((data) => setExecutives(data));
-    }, []);
+  useEffect(() => {
+    fetchContent('adminTeamMember').then((data) => setExecutives(data));
+  }, []);
 
-    return (
-        <div className={EXECUTIVES_CONTAINER}>
-            <Heading underlineType={UnderlineTypes.GREEN_LONG_UNDERLINE}>Executives</Heading>
-            <div className={EXECUTIVE_LEVEL_CONTAINER}>
-                {executives.map((exec) => (
-                    <Avatar key={exec.name} {...exec} />
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div className={EXECUTIVES_CONTAINER}>
+      <Heading underlineType={UnderlineTypes.GREEN_LONG_UNDERLINE}>Executives</Heading>
+      <div className={EXECUTIVE_LEVEL_CONTAINER}>
+        {executives.map((exec) => (
+          <Avatar key={exec.name} {...exec} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Executives;
