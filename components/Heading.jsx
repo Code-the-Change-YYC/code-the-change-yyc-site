26a661260b2dcceb6b5d09e5f8234586981fd0b1
topanb11 <topanbudiman11@gmail.com>
2023-08-06 20:30:02 -0400

    add events banner section

--- components/Heading.jsx
+++ components/Heading.jsx
@@ -1,13 +1,14 @@
-import { UnderlineTypes } from "../utils/underlineType";
+import { UnderlineTypes } from '../utils/underlineType';
 
 /* eslint-disable @next/next/no-img-element */
-const HEADING_CONTAINER = "py-5 flex flex-col items-center";
+const HEADING_CONTAINER = 'py-5 flex flex-col items-center';
 
-const GREEN_LONG_UNDERLINE_DIR = "/svgs/long_heading_underline.svg";
-const GREEN_SHORT_UNDERLINE_DIR = "/svgs/short_heading_underline.svg";
-const PURPLE_SHORT_UNDERLINE_DIR = "/svgs/timeline/purple_heading_underline.svg";
-const WHITE_SINGLE_LINE_DIR = "/svgs/white_long_underline.svg";
-const WHITE_CURLY_LINE_DIR = "/svgs/white_alumni_underline.svg";
+const GREEN_LONG_UNDERLINE_DIR = '/svgs/long_heading_underline.svg';
+const GREEN_SHORT_UNDERLINE_DIR = '/svgs/short_heading_underline.svg';
+const PURPLE_SHORT_UNDERLINE_DIR = '/svgs/timeline/purple_heading_underline.svg';
+const WHITE_SINGLE_LINE_DIR = '/svgs/white_long_underline.svg';
+const WHITE_CURLY_LINE_DIR = '/svgs/white_alumni_underline.svg';
+const BEIGE_SHORT_UNDERLINE_DIR = '/svgs/beige_underline.svg';
 
 const Heading = ({ classes, underlineType = UnderlineTypes.GREEN_SHORT_UNDERLINE, children }) => {
   const getUnderline = () => {
@@ -22,6 +23,8 @@ const Heading = ({ classes, underlineType = UnderlineTypes.GREEN_SHORT_UNDERLINE
         return WHITE_SINGLE_LINE_DIR;
       case UnderlineTypes.WHITE_CURLY_UNDERLINE:
         return WHITE_CURLY_LINE_DIR;
+      case UnderlineTypes.BEIGE_SHORT_UNDERLINE:
+        return BEIGE_SHORT_UNDERLINE_DIR;
     }
   };
 
---
+++ components/UpcomingEvents.jsx
@@ -0,0 +1,9 @@
+const UpcomingEvents = () => {
+  return (
+    <div>
+      <h1>Upcoming Events</h1>
+    </div>
+  );
+};
+
+export default UpcomingEvents;
