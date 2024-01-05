fdea71229055cb98b85b4418d713b9845bf3cdec
topanb11 <topanbudiman11@gmail.com>
2024-01-05 09:32:27 -0700

    wip

--- components/UpcomingEvents.jsx
+++ components/UpcomingEvents.jsx
@@ -1,7 +1,17 @@
+import { UnderlineTypes } from '../utils/underlineType';
+import Heading from './Heading';
+import EventCarousel from './EventsCarousel';
+
 const UpcomingEvents = () => {
   return (
-    <div>
-      <h1>Upcoming Events</h1>
+    <div className="flex flex-col items-center bg-orange h-[80vh] z-0">
+      <div className="flex items-baseline space-x-4">
+        <h1 className="text-white text-3xl font-medium">Upcoming</h1>
+        <Heading classes="text-white text-[2.25rem]" underlineType={UnderlineTypes.PURPLE_SHORT_UNDERLINE}>
+          Events
+        </Heading>
+      </div>
+      <EventCarousel />
     </div>
   );
 };
