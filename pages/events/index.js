c284574d4acdddd3dd62da0885bfd056450158b8
Burton Jong <burtonjong05@gmail.com>
2024-01-08 11:21:31 -0700

    Events page is no longer hardcoded and now is a component

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,18 +1,11 @@
+import EventsPlaceholder from '../../components/EventsPlaceholder';
+
 const Events = () => {
+  const placeholder = 'Events page';
   return (
-    <div className="flex flex-col w-full h-screen mt-5 md:mt-0 lg:mt-0 items-center">
-      {/* 
-			For each section here, try to do it as a component then just import here so it's easier to work on.
-			Below are just sample names, if you think it should use a better name, feel free to change it
-			
-			<UpcomingEvents/>
-			<HackathonTimeline/>
-			<PastEvents/>
-		*/}
-      <h1 className="m-auto text-3xl text-center">
-        We`re currently building the <b>Events Page</b> so stay tuned! 👀
-      </h1>
-    </div>
+    <>
+      <EventsPlaceholder placeholder={placeholder} />
+    </>
   );
 };
 
