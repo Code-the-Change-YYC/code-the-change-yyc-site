a96097e241c9d2b9f8fc549aa9639b525e7b16a0
Natalie <natalie.yeung16@gmail.com>
2024-01-11 11:32:15 -0700

    Add large screen responsiveness to upcoming events

--- pages/events/index.js
+++ pages/events/index.js
@@ -5,7 +5,8 @@ import UpcomingEvents from '../../components/UpcomingEvents';
 
 const Events = () => {
   return (
-    <div className="flex flex-col w-full mt-5 md:mt-0 lg:mt-0">
+    // <div className="flex flex-col w-full mt-5 md:mt-0 lg:mt-0">
+    <div className="flex flex-col w-full mt-5 md:mt-0 lg:mt-0 ">
       {/* For each section here, try to do it as a component then just import here so it's easier to work on.
 			Below are just sample names, if you think it should use a better name, feel free to change it
 			
