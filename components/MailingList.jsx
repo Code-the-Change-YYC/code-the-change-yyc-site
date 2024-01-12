b53cbf356cc33868a2ec8ac37c694f0b0d70d00e
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2024-01-12 10:32:05 -0700

    add join mailing list section

---
+++ components/MailingList.jsx
@@ -0,0 +1,30 @@
+// import Image from 'next/image';
+import Link from 'next/link';
+import { FaEnvelope } from 'react-icons/fa';
+const MAILING_LIST_CONTAINER = 'bg-[#7055FD] flex flex-col text-center';
+const CONTENT_CONTAINER = 'flex flex-col pt-14 p-5 md:px-32 text-center';
+const HEADING_CONTAINER = 'flex flex-row text-center justify-center';
+const LINK_TO_MAILING_LIST =
+  'https://docs.google.com/forms/d/e/1FAIpQLSdyU9zSPHnu76UwbRI3MO49FttyESLtkUBcPWWIK3vLkdTe_Q/viewform';
+const MailingList = () => {
+  return (
+    <div className={MAILING_LIST_CONTAINER}>
+      <div className={CONTENT_CONTAINER}>
+        <div className={HEADING_CONTAINER}>
+          <h1 className="text-5xl font-semibold text-white pr-4 flex flex-col justify-center text-center items-center sm:flex-row space-x-2">
+            <button className="font-extrabold underline bg-[#FF6B54] rounded p-2 w-fit hover:opacity-85">
+              <Link href={LINK_TO_MAILING_LIST}>Join </Link>
+            </button>
+            <div className="">our Mailing list!</div>
+            <FaEnvelope size={45} color="white" className="hidden sm:block align-middle" />
+          </h1>
+        </div>
+        <div className="text-lg text-white font-semibold">The link to our mailing list is also in our link tree </div>
+        {/* <div className="flex flex-row items-center space-x-4">
+        </div> */}
+      </div>
+    </div>
+  );
+};
+
+export default MailingList;
