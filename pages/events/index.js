a66d950fd9400d7d51a0b4cbf61f7ac89be54007
hannagracec <hanna.cho@ucalgary.ca>
2024-01-15 16:25:31 -0700

    added past events carousel to events page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,10 +1,12 @@
 import EventsPlaceholder from '../../components/EventsPlaceholder';
+import PastEventsCarousel from '../../components/PastEventsCarousel';
 
 const Events = () => {
   const placeholder = 'Events page';
   return (
     <>
       <EventsPlaceholder placeholder={placeholder} />
+      <PastEventsCarousel />
     </>
   );
 };
