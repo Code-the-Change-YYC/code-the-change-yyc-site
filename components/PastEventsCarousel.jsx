df7c57bbce4b1d3e9d06f3c06933daa5dcbe22b7
hannagracec <hannacho847@gmail.com>
2024-01-18 21:29:53 -0700

    Changed double quotes to single quotes

--- components/PastEventsCarousel.jsx
+++ components/PastEventsCarousel.jsx
@@ -5,9 +5,9 @@ import TextSection from './TextSection';
 import Carousel from '../components/Carousel';
 import { PAST_EVENTS } from '../data/events';
 import styles from '../components/RolesCarousel.module.css';
-import Heading from "./Heading";
-import { UnderlineTypes } from "../utils/underlineType";
-import Link from "next/link";
+import Heading from './Heading';
+import { UnderlineTypes } from '../utils/underlineType';
+import Link from 'next/link';
 import 'swiper/css/navigation';
 
 const CAROUSEL_CONTAINER = styles.swiperContainer;
@@ -16,48 +16,48 @@ const LEFT_NAVIGATION_ARROW = styles.swiperButtonPrev;
 const RIGHT_NAVIGATION_ARROW = styles.swiperButtonNext;
 
 const TILE_CONTAINER = 'flex flex-col xl:h-[650px] lg:h-[700px] md:flex-column md:h-[820px] rounded-[1.875rem] p-[1.875rem] h-[900px]';
-const TILE_IMAGE_CONTAINER = "flex flex-row justify-center md:flex-row md:justify-around lg:justify-center relative";
+const TILE_IMAGE_CONTAINER = 'flex flex-row justify-center md:flex-row md:justify-around lg:justify-center relative';
 const TILE_TEXT_CONTAINER = 'basis-3/5 md:basis-3/5 lg:basis-3/5 text-center md:py-10 ';
 const TILE_TEXT_SPACING = 'lg:space-y-48 py-5';
 
 const PAST_EVENTS_CONTAINER = 'bg-[#00D3A9] pb-10';
 const PAST_EVENTS_CONTENT = 'px-10 md:px-24 lg:px-48 py-20';
-const PAST_EVENTS_HEADER = "flex flex-row";
+const PAST_EVENTS_HEADER = 'flex flex-row';
 
-const LONG_SQUIGGLY_LINE_SVG = "/svgs/events/long_events_squiggly_line.svg";
+const LONG_SQUIGGLY_LINE_SVG = '/svgs/events/long_events_squiggly_line.svg';
 
 const PastEventsTile = ({ event }) => (
   <Tile classes={TILE_CONTAINER}>
-    <div className="basis-3/5 md:basis-3/5 lg:basis-3/5 text-center md:py-10 sm:hidden">
-      <h2 className="text-4xl md:text-6xl xl:text-6xl font-medium italic">{event.name}</h2>
+    <div className='basis-3/5 md:basis-3/5 lg:basis-3/5 text-center md:py-10 sm:hidden'>
+      <h2 className='text-4xl md:text-6xl xl:text-6xl font-medium italic'>{event.name}</h2>
     </div>
     <div className={TILE_IMAGE_CONTAINER}>
       <div >
         <Image 
-          className="w-50 h-50 p-2 md:w-60 md:h-60 lg:w-62 lg:h-62 mx-1 lg:mx-3 rounded-3xl drop-shadow-md relative "
+          className='w-50 h-50 p-2 md:w-60 md:h-60 lg:w-62 lg:h-62 mx-1 lg:mx-3 rounded-3xl drop-shadow-md relative '
           src={event.img1} 
           alt={event.name} 
         />
       </div>
-      <div className="hidden md:block lg:block">
+      <div className='hidden md:block lg:block'>
         <Image 
-          className="w-40 h-40 p-2 md:w-60 md:h-60 lg:w-62 lg:h-62 mx-1 lg:mx-3 rounded-3xl drop-shadow-md relative"
+          className='w-40 h-40 p-2 md:w-60 md:h-60 lg:w-62 lg:h-62 mx-1 lg:mx-3 rounded-3xl drop-shadow-md relative'
           src={event.img2} 
           alt={event.name} 
         />
       </div>
-      <div className="hidden md:hidden lg:block">
+      <div className='hidden md:hidden lg:block'>
         <Image 
-          className="w-40 h-40 p-2 md:w-60 md:h-60 lg:w-92 lg:h-92 mx-1 lg:mx-3 rounded-3xl drop-shadow-md relative"
+          className='w-40 h-40 p-2 md:w-60 md:h-60 lg:w-92 lg:h-92 mx-1 lg:mx-3 rounded-3xl drop-shadow-md relative'
           src={event.img3} 
           alt={event.name} 
         />
       </div>
     </div>
     <div className={TILE_TEXT_CONTAINER}>
-      <h2 className="text-4xl md:text-6xl xl:text-6xl font-medium italic">{event.name}</h2>
+      <h2 className='text-4xl md:text-6xl xl:text-6xl font-medium italic'>{event.name}</h2>
       <div className={TILE_TEXT_SPACING}>
-        <p className="text-[1.5em] md:text-[2rem] xl:text-[2rem] font-[500] py-3">{event.blurb}</p>
+        <p className='text-[1.5em] md:text-[2rem] xl:text-[2rem] font-[500] py-3'>{event.blurb}</p>
       </div>
     </div>
   </Tile>
@@ -68,10 +68,10 @@ const PastEventsCarousel = () => {
     <div className={PAST_EVENTS_CONTAINER}>
       <div className={PAST_EVENTS_CONTENT}>
         <div className={PAST_EVENTS_HEADER}>
-          <Heading classes="text-white" underlineType={UnderlineTypes.PURPLE_SHORT_UNDERLINE}>Past Events</Heading>
+          <Heading classes='text-white' underlineType={UnderlineTypes.PURPLE_SHORT_UNDERLINE}>Past Events</Heading>
         </div>
-        <TextSection classes="text-white">Here is an archive of some of the cool events we have held in the past! Whether you are interested in joining our team or attending one of our events,{" "}
-          <Link href="/contact" className="text-[#7055FD]">please feel free to reach out to us!</Link>
+        <TextSection classes='text-white'>Here is an archive of some of the cool events we have held in the past! Whether you are interested in joining our team or attending one of our events,{' '}
+          <Link href='/contact' className='text-[#7055FD]'>please feel free to reach out to us!</Link>
         </TextSection>
         <div className={CAROUSEL_CONTAINER}>
           <div className={`${ARROW_INIT.left} ${LEFT_NAVIGATION_ARROW}`}></div>
@@ -85,8 +85,8 @@ const PastEventsCarousel = () => {
           <div className={`${ARROW_INIT.right} ${RIGHT_NAVIGATION_ARROW}`}></div>
         </div>
       </div>
-      <div className="my-10 w-full h-48 xl:h-64 relative">
-        <Image src={LONG_SQUIGGLY_LINE_SVG} alt="" layout="fill" objectFit="cover" />
+      <div className='my-10 w-full h-48 xl:h-64 relative'>
+        <Image src={LONG_SQUIGGLY_LINE_SVG} alt='' layout='fill' objectFit='cover' />
       </div>
     </div>
   );
