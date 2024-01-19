53e6ded89cc119d9986adfc22ed0fc5ce61e8cb2
hannagracec <hannacho847@gmail.com>
2024-01-18 20:40:44 -0700

    Removed events placeholder

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,11 +1,8 @@
-import EventsPlaceholder from '../../components/EventsPlaceholder';
 import PastEventsCarousel from '../../components/PastEventsCarousel';
 
 const Events = () => {
-  const placeholder = 'Events page';
   return (
     <>
-      <EventsPlaceholder placeholder={placeholder} />
       <PastEventsCarousel />
     </>
   );
