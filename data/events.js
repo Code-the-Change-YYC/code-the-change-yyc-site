d8f433c5b328f13ef38ad2b9eaeeda664a4d613e
hannagracec <hannacho847@gmail.com>
2024-01-18 20:48:30 -0700

    Fixed syntax

--- data/events.js
+++ data/events.js
@@ -2,18 +2,18 @@ import HackathonWork from "../public/images/events/img1.jpg";
 import HackathonSpeech from "../public/images/events/img2.jpg";
 import HackathonSpeechTwo from "../public/images/events/img3.jpg";
 import HackathonJudges from "../public/images/events/img4.jpg";
-import networkingONE from "../public/images/events/past-events/networking_1.png"
-import networkingTWO from "../public/images/events/past-events/networking_2.jpg"
-import networkingTHREE from "../public/images/events/past-events/networking_3.jpg"
-import OpenSourceONE from "../public/images/events/past-events/open_source_1.png"
-import OpenSourceTWO from "../public/images/events/past-events/open_source_2.png"
-import OpenSourceTHREE from "../public/images/events/past-events/open_source_3.png"
-import ResponsibleAIone from "../public/images/events/past-events/responsible_AI_1.jpg"
-import ResponsibleAItwo from "../public/images/events/past-events/responsible_AI_2.jpg"
-import ResponsibleAIthree from "../public/images/events/past-events/responsible_AI_3.jpg"
-import RoastONE from "../public/images/events/past-events/resume_1.jpg"
-import RoastTWO from "../public/images/events/past-events/resume_2.jpg"
-import RoastTHREE from "../public/images/events/past-events/resume_3.jpg"
+import networkingONE from "../public/images/events/past-events/networking_1.png";
+import networkingTWO from "../public/images/events/past-events/networking_2.jpg";
+import networkingTHREE from "../public/images/events/past-events/networking_3.jpg";
+import OpenSourceONE from "../public/images/events/past-events/open_source_1.png";
+import OpenSourceTWO from "../public/images/events/past-events/open_source_2.png";
+import OpenSourceTHREE from "../public/images/events/past-events/open_source_3.png";
+import ResponsibleAIone from "../public/images/events/past-events/responsible_AI_1.jpg";
+import ResponsibleAItwo from "../public/images/events/past-events/responsible_AI_2.jpg";
+import ResponsibleAIthree from "../public/images/events/past-events/responsible_AI_3.jpg";
+import RoastONE from "../public/images/events/past-events/resume_1.jpg";
+import RoastTWO from "../public/images/events/past-events/resume_2.jpg";
+import RoastTHREE from "../public/images/events/past-events/resume_3.jpg";
 
 export const EVENTS_IMAGES = [
   {
@@ -63,4 +63,4 @@ export const PAST_EVENTS = [
     img3: networkingTHREE,
     blurb: "We hosted a panel discussion with tech industry leaders around Calgary! The discussed what to learn outside of the standard curriculum, how to market yourself to the industry, and what to expect at interviews!",
   },
-]
+];
