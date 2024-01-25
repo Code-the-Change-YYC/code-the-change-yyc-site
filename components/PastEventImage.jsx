a44286f61cd35b83a65024f713798476d0f8051b
hannagracec <hannacho847@gmail.com>
2024-01-24 18:58:37 -0700

    Made tile size fixed on mobile

--- components/PastEventImage.jsx
+++ components/PastEventImage.jsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 
-const PAST_EVENTS_IMAGE = 'w-50 h-50 p-2 md:w-60 md:h-60 lg:w-92 lg:h-92 mx-1 lg:mx-3 rounded-3xl drop-shadow-md relative';
+const PAST_EVENTS_IMAGE = 'w-65 h-65 p-2 md:w-60 md:h-60 lg:w-92 lg:h-92 mx-1 lg:mx-3 rounded-3xl drop-shadow-md relative';
 
 const PastEventImage = ({img, name}) => {
   return (
