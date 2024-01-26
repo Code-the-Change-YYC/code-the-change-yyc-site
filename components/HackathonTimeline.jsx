b7f0472c14d0fc55153a3cca70da7c0bb8b02efd
hannagracec <hannacho847@gmail.com>
2024-01-25 20:25:32 -0700

    Separated mapping into its own component

--- components/HackathonTimeline.jsx
+++ components/HackathonTimeline.jsx
@@ -5,7 +5,28 @@ import { HACKATHONS } from '../data/hackathons';
 
 const HACKATHON_TIMELINE_CONTAINER = 'bg-white';
 const HACKATHON_TIMELINE_HEADER = 'text-center'
+const HACKATHON_TIMELINE_CONTENT = 'flex-col justify-center text-center text-xl px-[50px] font-bold mb-[70px]'
+const HACKATHON_TITLE_YEAR = 'text-5xl pb-[15px] italic'
+const HACKATHON_IMAGE = 'w-65 h-70'
 
+const HackathonSection = ({ hackathon }) => {
+    return (
+        <div className={HACKATHON_TIMELINE_CONTENT}>
+            <h1 className={HACKATHON_TITLE_YEAR}>
+                 {hackathon.year}
+            </h1>
+            <div className={HACKATHON_IMAGE}>
+                <Image className='object-cover rounded-xl' src={hackathon.img} />
+            </div>
+            <div>
+                <p className='text-left my-[15px] text-[#FF6B54]'>
+                    Topics:  {hackathon.topic}
+                </p>
+                <p className='font-medium'>{hackathon.blurb}</p>
+            </div>
+        </div>
+    );
+}
 
 const HackathonTimeline = () => {
     return (
@@ -15,18 +36,7 @@ const HackathonTimeline = () => {
                     <Heading classes='text-black' underlineType={UnderlineTypes.GREEN_LONG_UNDERLINE}>Hack the Change</Heading>
                 </div>
                 {HACKATHONS.map((hackathon) => (
-                    <div className='flex-col justify-center text-center text-xl px-[50px] font-bold mb-[70px]'>
-                        <h1 className='text-5xl pb-[15px] italic'>
-                            {hackathon.year}
-                        </h1>
-                        <div className='w-65 h-70'>
-                            <Image className='object-cover' src={hackathon.img} />
-                        </div>
-                        <p className='text-left my-[15px] text-[#FF4D6F]'>
-                            Topics:  {hackathon.topic}
-                        </p>
-                        <p className='font-medium'>{hackathon.blurb}</p>
-                    </div>
+                    <HackathonSection hackathon={hackathon} />
                 ))}
             </div>
         </div>
--- data/hackathons.js
+++ data/hackathons.js
@@ -5,13 +5,13 @@ import Hackathon2023 from '../public/images/events/hackathons/htc2023.jpg'
 export const HACKATHONS = [
     {
         year: '2022',
-        img: Hackathon2021,
+        img: Hackathon2022,
         topic: 'Education, inequality, and sustainable living',
         blurb: 'Our biggest hackathon to date! It saw students from all over Canada come up with innovative solutions.'
     },
     {
         year: '2021',
-        img: Hackathon2022,
+        img: Hackathon2021,
         topic: 'Health and Wellness',
         blurb: 'Due to COVID, this hackathon was held online, but still had an impressive turnout.'
     },
