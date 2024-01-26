00a6288bdcaca4dda9a49bd0785b549d2377cec1
hannagracec <hannacho847@gmail.com>
2024-01-25 19:04:46 -0700

    Added hackathon images

--- data/hackathons.js
+++ data/hackathons.js
@@ -1,21 +1,23 @@
-import Hackathon from "../public/images/events/img2.jpg"
+import Hackathon2021 from '../public/images/events/hackathons/htc2021.png'
+import Hackathon2022 from '../public/images/events/hackathons/htc2022.JPG'
+import Hackathon2023 from '../public/images/events/hackathons/htc2023.jpg'
 
 export const HACKATHONS = [
     {
         year: '2022',
-        img: Hackathon,
+        img: Hackathon2021,
         topic: 'Education, inequality, and sustainable living',
         blurb: 'Our biggest hackathon to date! It saw students from all over Canada come up with innovative solutions.'
     },
     {
         year: '2021',
-        img: Hackathon,
+        img: Hackathon2022,
         topic: 'Health and Wellness',
         blurb: 'Due to COVID, this hackathon was held online, but still had an impressive turnout.'
     },
     {
         year: '2019',
-        img: Hackathon,
+        img: Hackathon2023,
         topic: 'Climate Action',
         blurb: 'Our first ever hackathon! Shout out to Benevity for letting us hold it there.'
     }
