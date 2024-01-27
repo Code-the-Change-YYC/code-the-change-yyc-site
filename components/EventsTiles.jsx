1fb64951f6251284918fa66a1714d94da0ac90f7
Natalie <natalie.yeung16@gmail.com>
2024-01-26 18:43:41 -0700

    Connect contentful to desktop upcoming events page

--- components/EventsTiles.jsx
+++ components/EventsTiles.jsx
@@ -1,56 +1,67 @@
+import React, { useEffect, useState } from 'react';
 import TextSection from './TextSection';
 import Heading from './Heading';
+import { fetchContent } from '../api/apiRoot';
 
-const events = [
-  {
-    id: 1,
-    name: 'Event Name',
-    description: 'Our biggest event to date It saw students from all over Canada come up with innovative solutions',
-  },
-  {
-    id: 2,
-    name: 'Event Name',
-    description: 'Our biggest event to date It saw students from all over Canada come up with innovative solutions',
-  },
-  {
-    id: 3,
-    name: 'Event Name',
-    description:
-      'Our biggest event to date It saw students from all over Canada come up with innovative solution kfhewiuo akjlsfhakjdlf hsdklf hsadklf hsdakjl hasdk',
-  },
-];
+// const events = [
+//   {
+//     id: 1,
+//     name: 'Event Name',
+//     description: 'Our biggest event to date It saw students from all over Canada come up with innovative solutions',
+//   },
+//   {
+//     id: 2,
+//     name: 'Event Name',
+//     description: 'Our biggest event to date It saw students from all over Canada come up with innovative solutions',
+//   },
+//   {
+//     id: 3,
+//     name: 'Event Name',
+//     description:
+//       'Our biggest event to date It saw students from all over Canada come up with innovative solution kfhewiuo akjlsfhakjdlf hsdklf hsadklf hsdakjl hasdk',
+//   },
+// ];
 
 const EVENTS_TILES_CONTAINER = 'w-4/5 mx-auto flex flex-wrap md:flex hidden';
 const EVENT_TILE = 'flex flex-col h-96 bg-white rounded-3xl items-center px-4 justify-evenly';
 
-const EventTile = ({ name, description }) => {
+const EventTile = ({ eventName, description }) => {
+  const descriptionText = description.content[0].content[0].value;
   return (
     <div className={EVENT_TILE}>
       <Heading classes="text-3xl font-medium" underlineType="None">
-        {name}
+        {eventName}
       </Heading>
       <div className="bg-lilac h-24 w-24"></div>
-      <TextSection classes="text-sm">{description}</TextSection>
+      <TextSection classes="text-sm">{descriptionText}</TextSection>
     </div>
   );
 };
 
 const EventsTiles = () => {
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    fetchContent('upcomingEvents').then((data) => setEvents(data));
+  }, []);
+
   return (
     <div className={EVENTS_TILES_CONTAINER}>
       {/* First Column (One Large Row) */}
       <div className="w-full lg:w-1/2 p-4">
         <div className={`${EVENT_TILE} h-full`}>
-          <EventTile {...events[0]} />
+          {events.slice(0, 1).map((event) => (
+            <EventTile key={event.id} {...event} />
+          ))}
         </div>
       </div>
-
       {/* Second Column (Two Smaller Rows) */}
       <div className="w-full lg:w-1/2 flex flex-wrap">
-        {events.slice(1, 3).map((values, index) => (
-          <div key={index} className="w-full lg:h-1/2 p-4">
+        {events.slice(1, 3).map((event) => (
+          <div className="w-full lg:h-1/2 p-4">
             <div className={`${EVENT_TILE} `}>
-              <EventTile {...values} />
+              {/* <EventTile {...values} /> */}
+              <EventTile key={event.id} {...event} />
             </div>
           </div>
         ))}
