ea2b2a4d8719d125e292d81a57495e99916540bd
Natalie <natalie.yeung16@gmail.com>
2024-01-26 18:43:41 -0700

    Connect contentful to desktop upcoming events page

--- components/EventsCarousel.jsx
+++ components/EventsCarousel.jsx
@@ -29,6 +29,7 @@ const EVENT_TILE = 'flex flex-col h-96 bg-white rounded-3xl items-center px-4 ju
 
 const EventTile = ({ eventName, description }) => {
   const descriptionText = description.content[0].content[0].value || 'Invalid description';
+
   return (
     <div className={EVENT_TILE}>
       <Heading classes="text-3xl font-medium" underlineType="None">
