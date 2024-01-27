9763194cf8ba4a8a844b4bfd65c819d8f333df00
hannagracec <hannacho847@gmail.com>
2024-01-26 19:42:10 -0700

    Fixed syntax

--- components/HackathonTimeline.jsx
+++ components/HackathonTimeline.jsx
@@ -4,43 +4,43 @@ import { UnderlineTypes } from '../utils/underlineType';
 import { HACKATHONS } from '../data/hackathons';
 
 const HACKATHON_TIMELINE_CONTAINER = 'bg-white';
-const HACKATHON_TIMELINE_HEADER = 'text-center'
-const HACKATHON_TIMELINE_CONTENT = 'flex-col text-center text-xl px-[50px] font-bold mb-[70px] md:flex-row'
-const HACKATHON_TITLE_YEAR = 'text-5xl pb-[15px] italic'
-const HACKATHON_IMAGE = 'border-[8px] border-[#00D3A9] object-cover rounded-xl max-w-[400px]'
+const HACKATHON_TIMELINE_HEADER = 'text-center';
+const HACKATHON_TIMELINE_CONTENT = 'flex-col text-center text-xl px-[50px] font-bold mb-[70px] md:flex-row';
+const HACKATHON_TITLE_YEAR = 'text-5xl pb-[15px] italic';
+const HACKATHON_IMAGE = 'border-[8px] border-[#00D3A9] object-cover rounded-xl max-w-[400px]';
 
 const HackathonSection = ({ hackathon }) => {
-    return (
-        <div className={HACKATHON_TIMELINE_CONTENT}>
-            <div>
-                <h1 className={HACKATHON_TITLE_YEAR}>
-                    {hackathon.year}
-                </h1>
-                    <Image className={HACKATHON_IMAGE} src={hackathon.img} />
-                </div>
-            <div>
-                <p className='text-left my-[15px] text-[#FF6B54]'>
-                    Topics:  {hackathon.topic}
-                </p>
-                <p className='font-medium text-[#00D3A9]'>{hackathon.blurb}</p>
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className={HACKATHON_TIMELINE_CONTENT}>
+      <div>
+        <h1 className={HACKATHON_TITLE_YEAR}>
+          {hackathon.year}
+        </h1>
+        <Image className={HACKATHON_IMAGE} src={hackathon.img} alt={hackathon.title} />
+      </div>
+      <div>
+        <p className='text-left my-[15px] text-[#FF6B54]'>
+          Topics:  {hackathon.topic}
+        </p>
+        <p className='font-medium text-[#00D3A9]'>{hackathon.blurb}</p>
+      </div>
+    </div>
+  );
+};
 
 const HackathonTimeline = () => {
-    return (
-        <div className={HACKATHON_TIMELINE_CONTAINER}>
-            <div>
-                <div className={HACKATHON_TIMELINE_HEADER}>
-                    <Heading classes='text-black' underlineType={UnderlineTypes.GREEN_LONG_UNDERLINE}>Hack the Change</Heading>
-                </div>
-                {HACKATHONS.map((hackathon) => (
-                    <HackathonSection hackathon={hackathon} />
-                ))}
-            </div>
+  return (
+    <div className={HACKATHON_TIMELINE_CONTAINER}>
+      <div>
+        <div className={HACKATHON_TIMELINE_HEADER}>
+          <Heading classes='text-black' underlineType={UnderlineTypes.GREEN_LONG_UNDERLINE}>Hack the Change</Heading>
         </div>
-    );
-}
+        {HACKATHONS.map((hackathon, index) => (
+          <HackathonSection hackathon={hackathon} key={index}/>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default HackathonTimeline;
--- data/hackathons.js
+++ data/hackathons.js
@@ -1,24 +1,28 @@
-import Hackathon2021 from '../public/images/events/hackathons/htc2021.png'
-import Hackathon2022 from '../public/images/events/hackathons/htc2022.JPG'
-import Hackathon2023 from '../public/images/events/hackathons/htc2023.jpg'
+import Hackathon2021 from '../public/images/events/hackathons/htc2021.png';
+import Hackathon2022 from '../public/images/events/hackathons/htc2022.JPG';
+import Hackathon2023 from '../public/images/events/hackathons/htc2023.jpg';
 
 export const HACKATHONS = [
-    {
-        year: '2022',
-        img: Hackathon2022,
-        topic: 'Education, inequality, and sustainable living',
-        blurb: 'Our biggest hackathon to date! It saw students from all over Canada come up with innovative solutions.'
-    },
-    {
-        year: '2021',
-        img: Hackathon2021,
-        topic: 'Health and Wellness',
-        blurb: 'Due to COVID, this hackathon was held online, but still had an impressive turnout.'
-    },
-    {
-        year: '2019',
-        img: Hackathon2023,
-        topic: 'Climate Action',
-        blurb: 'Our first ever hackathon! Shout out to Benevity for letting us hold it there.'
-    }
-]
+  {
+    title: '2022 Hackathon',
+    year: '2022',
+    img: Hackathon2022,
+    topic: 'Education, inequality, and sustainable living',
+    blurb: 'Our biggest hackathon to date! It saw students from all over Canada come up with innovative solutions.'
+  },
+  {
+    title: '2021 Hackathon',
+    year: '2021',
+    img: Hackathon2021,
+    topic: 'Health and Wellness',
+    blurb: 'Due to COVID, this hackathon was held online, but still had an impressive turnout.'
+  },
+  {
+    title: '2019 Hackathon',
+    year: '2019',
+    img: Hackathon2023,
+    topic: 'Climate Action',
+    blurb: 'Our first ever hackathon! Shout out to Benevity for letting us hold it there.'
+  }
+];
+
