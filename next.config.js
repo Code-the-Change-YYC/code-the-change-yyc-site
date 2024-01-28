674b1666d278896f5f9125bba2acaa42a716a33b
Natalie <natalie.yeung16@gmail.com>
2024-01-28 11:23:17 -0700

    Fix public access token error

--- next.config.js
+++ next.config.js
@@ -5,10 +5,7 @@ require('dotenv').config();
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // PUBLIC_ACCESS_TOKEN: process.env.NEXT_PUBLIC_ACCESS_TOKEN,
-  env: {
-    PUBLIC_ACCESS_TOKEN: process.env.NEXT_PUBLIC_ACCESS_TOKEN,
-  },
+  PUBLIC_ACCESS_TOKEN: process.env.NEXT_PUBLIC_ACCESS_TOKEN,
   // Allow images fromm contentful
   images: {
     domains: ['images.ctfassets.net'],
