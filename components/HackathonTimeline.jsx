2b4fb6a9029545beb4b3423a57f0c3c636e27d4e
hannagracec <hannacho847@gmail.com>
2024-01-28 20:50:05 -0700

    Updated mobile view styling

--- components/HackathonTimeline.jsx
+++ components/HackathonTimeline.jsx
@@ -4,11 +4,11 @@ import { UnderlineTypes } from '../utils/underlineType';
 import { HACKATHONS } from '../data/hackathons';
 
 const HACKATHON_TIMELINE_CONTAINER = 'flex justify-center bg-white';
-const HACKATHON_TIMELINE_CONTENT = 'w-[500px] md:w-[1100px]';
-const HACKATHON_TIMELINE_HEADER = 'flex justify-center my-[30px] md:justify-start md:mx-[50px] md:my-[60px]';
-const HACKATHON_EVENT_INFO = 'flex text-center text-xl px-[50px] font-bold mb-[70px] md:items-center flex-col';
+const HACKATHON_TIMELINE_CONTENT = 'max-w-[500px] w-[80vw] md:max-w-[1100px]';
+const HACKATHON_TIMELINE_HEADER = 'md:justify-start md:my-[60px] my-[30px] flex justify-center';
+const HACKATHON_EVENT_INFO = 'flex text-center text-xl font-bold mb-[70px] md:items-center flex-col items-center';
 const HACKATHON_TITLE_YEAR = 'text-5xl pb-[15px] italic md:text-4xl';
-const HACKATHON_IMAGE = 'object-cover md:rounded-xl max-w-[400px] md:w-[450px] md:h-[250px] md:shadow-[15px_15px_0px_0px_#00D3A9]';
+const HACKATHON_IMAGE = 'object-cover w-[80vw] md:rounded-xl md:w-[450px] md:h-[250px] md:shadow-[15px_15px_0px_0px_#00D3A9]';
 const HACKATHON_TEXT = 'flex-col md:w-[400px] md:mx-[40px] leading-[1] text-[20px]';
 const HACKATHON_TOPIC_TEXT = 'flex text-left my-[15px] text-[#FF4D6F] md:text-[#FF6B54] md:my-1 md:font-extrabold md:text-[25px]';
 
@@ -50,7 +50,7 @@ const HackathonTimeline = () => {
     <div className={HACKATHON_TIMELINE_CONTAINER}>
       <div className={HACKATHON_TIMELINE_CONTENT}>
         <div className={HACKATHON_TIMELINE_HEADER}>
-          <Heading classes='text-black' underlineType={UnderlineTypes.GREEN_LONG_UNDERLINE}>Hack the Change</Heading>
+          <Heading underlineType={UnderlineTypes.GREEN_LONG_UNDERLINE}>Hack the Change</Heading>
         </div>
         {HACKATHONS.map((hackathon, index) => (
           <HackathonSection hackathon={hackathon} key={index} index={index}/>
