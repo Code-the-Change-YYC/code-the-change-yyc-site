38d4bb3cdf4d5ec90d9083f27577d3de0e50da63
hannagracec <hannacho847@gmail.com>
2024-01-30 12:37:16 -0700

    Made code styling more concise

--- components/HackathonTimeline.jsx
+++ components/HackathonTimeline.jsx
@@ -6,6 +6,7 @@ import { HACKATHONS } from '../data/hackathons';
 const HACKATHON_TIMELINE_CONTAINER = 'flex justify-center bg-white';
 const HACKATHON_TIMELINE_CONTENT = 'max-w-[500px] w-[80vw] md:max-w-[1100px]';
 const HACKATHON_TIMELINE_HEADER = 'md:justify-start md:my-[60px] my-[30px] flex justify-center';
+
 const HACKATHON_EVENT_INFO = 'flex text-center text-xl font-bold mb-[70px] md:items-center flex-col items-center';
 const HACKATHON_TITLE_YEAR = 'text-5xl pb-[15px] italic md:text-4xl';
 const HACKATHON_IMAGE = 'object-cover w-[80vw] md:rounded-xl md:w-[450px] md:h-[250px] md:shadow-[15px_15px_0px_0px_#00D3A9]';
@@ -13,18 +14,15 @@ const HACKATHON_TEXT = 'flex-col md:w-[400px] md:mx-[40px] leading-[1] text-[20p
 const HACKATHON_TOPIC_TEXT = 'flex text-left my-[15px] text-[#FF4D6F] md:text-[#FF6B54] md:my-1 md:font-extrabold md:text-[25px]';
 
 const HackathonSection = ({ hackathon, index }) => {
-  let sectionInfoStyling = '';
-  let sectionTextStyling = '';
-  let sectionTopicStyling = '';
+  let sectionInfoStyling = 'md:flex-row-reverse';
+  let sectionTextStyling = 'md:text-right';
+  let sectionTopicStyling = 'md:text-right md:justify-end';
 
   if(index % 2 == 0) {
-    sectionInfoStyling += 'md:flex-row md:justify-start';
-    sectionTextStyling += 'md:text-left';
-  } else {
-    sectionInfoStyling += 'md:flex-row-reverse';
-    sectionTextStyling += 'md:text-right';
-    sectionTopicStyling += 'md:text-right md:justify-end';
-  }
+    sectionInfoStyling = 'md:flex-row md:justify-start';
+    sectionTextStyling = 'md:text-left';
+    sectionTopicStyling = '';
+  } 
 
   return (
     <div className={`${HACKATHON_EVENT_INFO} ${sectionInfoStyling}`}>
