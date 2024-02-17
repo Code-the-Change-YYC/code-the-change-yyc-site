4ed7c33540507ef38a37c28ce033141c926e8a94
Natalie <natalie.yeung16@gmail.com>
2024-02-17 10:20:07 -0700

    Clean up code

--- pages/events/index.js
+++ pages/events/index.js
@@ -13,14 +13,6 @@ export async function getStaticProps() {
 const Events = ({ event, pastEvent }) => {
   return (
     <div className="flex flex-col w-full mt-5 md:mt-0 lg:mt-0 ">
-      {/* For each section here, try to do it as a component then just import here so it's easier to work on.
-			Below are just sample names, if you think it should use a better name, feel free to change it
-			
-	
-		
-      <h1 className="m-auto text-3xl text-center">
-        We`re currently building the <b>Events Page</b> so stay tuned! 👀
-      </h1> */}
       <EventsBanner />
       <UpcomingEvents event={event} />
       <HackathonTimeline />
