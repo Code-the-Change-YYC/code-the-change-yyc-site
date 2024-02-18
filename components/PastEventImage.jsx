cb8e197f5abf43bb3c95c717763020c10c14462f
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2024-02-18 10:51:25 -0700

    suggested changes for PR

--- components/PastEventImage.jsx
+++ components/PastEventImage.jsx
@@ -1,46 +1,36 @@
 import Image from 'next/image';
-
+import { rgbDataURL } from '../utils/blurImage';
 const PAST_EVENTS_IMAGE =
-  'w-65 h-65 p-2 md:w-60 md:h-60 md:mx-4 lg:w-92 lg:h-92 lg:w-1/3 lg:mx-1 rounded-3xl drop-shadow-md relative';
+  'w-65 h-65 md:w-60 md:h-60 md:mx-4 lg:w-92 lg:h-92 lg:w-1/3 lg:mx-1 object-cover rounded-3xl drop-shadow-md relative';
 
+const PastEventImageComponent = ({ eventImages }) => {
+  return eventImages.map((image, index) => (
+    <Image
+      key={index}
+      className={`${PAST_EVENTS_IMAGE}`}
+      src={`https:${image.fields.file.url}`}
+      alt={image.fields.title}
+      width={image.fields.file.details.image.width}
+      height={image.fields.file.details.image.height}
+      placeholder="blur"
+      blurDataURL={rgbDataURL()}
+    />
+  ));
+};
 const PastEventImage = ({ images }) => {
   return (
     <div className="flex overflow-x">
       {/* Display only the first image on small screens */}
       <div className="md:hidden lg:hidden">
-        <Image
-          className={PAST_EVENTS_IMAGE}
-          src={`https:${images[0].fields.file.url}`}
-          alt={images[0].fields.title}
-          width={images[0].fields.file.details.image.width}
-          height={images[0].fields.file.details.image.height}
-        />
+        <PastEventImageComponent eventImages={[images[0]]} />
       </div>
       {/* Display only the first two images on medium screens */}
       <div className="hidden md:flex lg:hidden">
-        {images.slice(0, 2).map((image, index) => (
-          <Image
-            key={index}
-            className={`${PAST_EVENTS_IMAGE}`}
-            src={`https:${image.fields.file.url}`}
-            alt={image.fields.title}
-            width={image.fields.file.details.image.width}
-            height={image.fields.file.details.image.height}
-          />
-        ))}
+        <PastEventImageComponent eventImages={images.slice(0, 2)} />
       </div>
       {/* Display all images on large screens */}
       <div className="hidden lg:flex">
-        {images.map((image, index) => (
-          <Image
-            key={index}
-            className={`${PAST_EVENTS_IMAGE}`}
-            src={`https:${image.fields.file.url}`}
-            alt={image.fields.title}
-            width={image.fields.file.details.image.width}
-            height={image.fields.file.details.image.height}
-          />
-        ))}
+        <PastEventImageComponent eventImages={images} />
       </div>
     </div>
   );
--- utils/blurImage.js
+++ utils/blurImage.js
@@ -6,7 +6,8 @@ const triplet = (e1, e2, e3) =>
   keyStr.charAt(((e2 & 15) << 2) | (e3 >> 6)) +
   keyStr.charAt(e3 & 63);
 
-export const rgbDataURL = (r, g, b) =>
+// Color defaults to grey
+export const rgbDataURL = (r = 192, g = 192, b = 192) =>
   `data:image/gif;base64,R0lGODlhAQABAPAA${
     triplet(0, r, g) + triplet(b, 255, 255)
   }/yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==`;
