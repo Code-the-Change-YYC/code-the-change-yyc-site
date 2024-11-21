3d98155f6b7af6924fd521fe7634fbd811c7e1a2
Yamyam <ethan.braum@gmail.com>
2024-11-20 23:27:44 -0700

    Fix upcoming events margin

--- components/UpcomingEvents.jsx
+++ components/UpcomingEvents.jsx
@@ -1,12 +1,12 @@
-import { UnderlineTypes } from '../utils/underlineType';
-import Heading from './Heading';
 import EventsCarousel from './EventsCarousel';
 import EventsTiles from './EventsTiles';
+import Heading from './Heading';
+import { UnderlineTypes } from '../utils/underlineType';
 
 const UpcomingEvents = ({ event }) => {
   return (
-    <div className="flex flex-col items-center bg-orange h-[80vh] md:h-[210vh] lg:h-[145vh] z-0">
-      <div className="flex items-baseline space-x-4">
+    <div className="flex flex-col items-center bg-orange z-0 pb-12 md:pb-16 lg:pb-20">
+      <div className="flex items-baseline space-x-4 mb-4">
         <h1 className="text-white text-3xl font-medium">Upcoming</h1>
         <Heading classes="text-white text-[2.25rem]" underlineType={UnderlineTypes.PURPLE_SHORT_UNDERLINE}>
           Events
