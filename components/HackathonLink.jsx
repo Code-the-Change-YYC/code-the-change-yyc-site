5346029024a7aaf3cfc566881c673503832df53a
abia-jahangir <abiajahangir1@gmail.com>
2025-01-28 22:01:37 -0700

    changed text format

--- components/HackathonLink.jsx
+++ components/HackathonLink.jsx
@@ -14,19 +14,23 @@ const HackathonLink = () => {
             Join the Change at Our Hackathons: Hack the Change
             </Heading>
         </div>
-        <TextSection classes="text-white pb-10 z-20 px-10 md:px-24 lg:px-48 md:py-4">
+        <TextSection classes="text-white pb-5 z-10 px-10 md:px-24 lg:px-48 md:py-4">
             Dive into collaborative innovation with Code the Change YYC's hackathons! Designed to empower 
             student developers and create real-world impact, our hackathons offer an exciting opportunity 
             to learn, build, and connect. Whether you're solving pressing challenges or sharpening your 
             skills, there's a place for everyone.
-            Explore upcoming events, resources, and more at {' '}
+        </TextSection>
+        <TextSection classes="text-white pb-5 z-10 px-10 md:px-24 lg:px-48 md:py-4 font-medium">
+            Explore upcoming events, resources, and more at: {' '}
+        </TextSection>
+        <TextSection classes="text-white pb-10 z-20 px-10 md:px-24 lg:px-48 md:py-4">
             <Link 
                 href="https://hackthechangeyyc.ca" 
                 target="_blank" 
                 rel="noopener noreferrer" 
                 className="text-blue font-semibold underline">
                 <span>hackthechangeyyc.ca</span>
-            </Link>.
+            </Link>
        </TextSection>
     </div>
     );
