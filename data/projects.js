ddf88a6790c2574af9d32b1b81189b2bae058b61
Abigia Debebe <abigia.debebe@ucalgary.ca>
2025-01-29 23:39:08 -0700

    Added ScubaSeas Project

--- data/projects.js
+++ data/projects.js
@@ -1,51 +1,60 @@
-import RubyForGood from "../public/images/projects/rfg.png";
-import YWCA from "../public/images/projects/ywca.png";
-import Mindfuel from "../public/images/projects/mindfuel.png";
-import AMHAC from "../public/images/projects/amhac.png";
-import AXS from "../public/images/projects/axs_map.png";
+import RubyForGood from '../public/images/projects/rfg.png';
+import YWCA from '../public/images/projects/ywca.png';
+import Mindfuel from '../public/images/projects/mindfuel.png';
+import AMHAC from '../public/images/projects/amhac.png';
+import AXS from '../public/images/projects/axs_map.png';
+import ScubaSeas from '../public/images/projects/ScubaSeas.png';
 
 export const projects = [
   {
     logo: RubyForGood,
-    name: "Ruby for Good CASA",
-    liveProjectLink: "https://casavolunteertracking.org/",
-    githubLink: "https://github.com/rubyforgood/casa/issues/1017",
+    name: 'Ruby for Good CASA',
+    liveProjectLink: 'https://casavolunteertracking.org/',
+    githubLink: 'https://github.com/rubyforgood/casa/issues/1017',
     descriptions: [
-      "Built an SMS Twilio integration within a volunteer management system for the nonprofit CASA, which serves foster youth in counties across America",
+      'Built an SMS Twilio integration within a volunteer management system for the nonprofit CASA, which serves foster youth in counties across America',
     ],
   },
   {
     logo: YWCA,
-    name: "E-learning Storefront",
+    name: 'E-learning Storefront',
     descriptions: [
-      "Creation and design of a storefront website with e-commerce integrated",
+      'Creation and design of a storefront website with e-commerce integrated',
       "Build APIs to interface with companies' different E-Learning platforms",
-      "Work with secure registration and authentication technologies",
+      'Work with secure registration and authentication technologies',
     ],
   },
   {
     logo: Mindfuel,
-    name: "Mindfuel System Redesign",
+    name: 'Mindfuel System Redesign',
     descriptions: [
-      "Build an activity board that contains new aggregated data everyday",
-      "Start from scratch or build on top of current system",
-      "Work with skilled developers from the Mindfuel team",
+      'Build an activity board that contains new aggregated data everyday',
+      'Start from scratch or build on top of current system',
+      'Work with skilled developers from the Mindfuel team',
     ],
   },
   {
     logo: AXS,
-    name: "AXS Map",
-    liveProjectLink: "https://www.axsmap.com/",
-    githubLink: "https://github.com/axsmap/app",
-    descriptions: ["A tool designed to help us map inclusion in communities and find more places for more people."],
+    name: 'AXS Map',
+    liveProjectLink: 'https://www.axsmap.com/',
+    githubLink: 'https://github.com/axsmap/app',
+    descriptions: ['A tool designed to help us map inclusion in communities and find more places for more people.'],
   },
   {
     logo: AMHAC,
-    name: "Alberta Mental Health Advocacy Coalition",
+    name: 'Alberta Mental Health Advocacy Coalition',
     descriptions: [
-      "Create and design the organization website and logo",
-      "Expand their online presence to reach more individuals",
-      "Good project for members who are at the beginning of their programming journey",
+      'Create and design the organization website and logo',
+      'Expand their online presence to reach more individuals',
+      'Good project for members who are at the beginning of their programming journey',
+    ],
+  },
+  {
+    logo: ScubaSeas,
+    name: 'ScubaSeas',
+    descriptions: [
+      'Open source mobile app that helps divers map out the entire ocean',
+      'Mobile App Developers needed to contribute to the open source project',
     ],
   },
 ];
