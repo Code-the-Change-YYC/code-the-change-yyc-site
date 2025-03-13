51a74556ba1196327b40a37710cb0c49a46f0750
abia-jahangir <abiajahangir1@gmail.com>
2025-03-13 14:07:49 -0600

    started on internal team. cant figure out linkedin icon colours

--- components/Avatar.jsx
+++ components/Avatar.jsx
@@ -4,7 +4,8 @@ import FontAwesomeLink from './FontAwesomeLink';
 
 const PROFILE_CONTAINER = 'flex flex-col items-center m-10';
 
-const Avatar = ({ name, linkedin, position, pfp, classes, prevRole }) => {
+
+const Avatar = ({ name, linkedin, position, pfp, classes, prevRole}) => {
   const img = 'https:' + pfp.fields.file.url;
   return (
     <div className={PROFILE_CONTAINER}>
@@ -16,7 +17,7 @@ const Avatar = ({ name, linkedin, position, pfp, classes, prevRole }) => {
       {prevRole && (
         <h2 className={`text-[#7055FD] text-xs sm:text-base font-medium mb-2 ${classes}`}>Previously: {prevRole}</h2>
       )}
-      {linkedin && <FontAwesomeLink username={linkedin} icon={faLinkedin} />}
+      {linkedin && <FontAwesomeLink username={linkedin} icon={faLinkedin} className={classes} />}
     </div>
   );
 };
