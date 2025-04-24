3843ca706eabef8db91ea05b275cba27ff8ff3c9
Abigia Debebe <abigia.debebe@ucalgary.ca>
2025-04-23 23:57:46 -0600

    unused imports removed

--- pages/our-team/index.js
+++ pages/our-team/index.js
@@ -1,7 +1,4 @@
 import UnderConstruction from '../../components/UnderConstruction';
-import InternalTeam from '../../components/InternalTeam';
-import TechnicalTeam from '../../components/TechnicalTeam';
-import MeetOurTeam from '../../components/MeetOurTeam';
 
 const OurTeam = () => {
   return (
