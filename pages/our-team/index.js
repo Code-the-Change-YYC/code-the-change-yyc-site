997df08f103bbf76d16de086d3e973245fe2c5f8
Abigia Debebe <abigia.debebe@ucalgary.ca>
2025-05-01 21:54:17 -0400

    Under construction added to team page

---
+++ pages/our-team/index.js
@@ -0,0 +1,11 @@
+import UnderConstruction from '../../components/UnderConstruction';
+
+const OurTeam = () => {
+  return (
+    <div>
+      <UnderConstruction />
+    </div>
+  );
+};
+
+export default OurTeam;
