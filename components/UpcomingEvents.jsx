eb928b91433185af3a1709c3157436cb573ae2a8
anthonyych4n <anthony168c@gmail.com>
2025-11-03 23:39:55 -0700

    spacing in events page

--- components/UpcomingEvents.jsx
+++ components/UpcomingEvents.jsx
@@ -1,11 +1,11 @@
+import { UnderlineTypes } from '../utils/underlineType';
 import EventsCarousel from './EventsCarousel';
 import EventsTiles from './EventsTiles';
 import Heading from './Heading';
-import { UnderlineTypes } from '../utils/underlineType';
 
 const UpcomingEvents = ({ upcomingEvent }) => {
   return (
-    <div className="flex flex-col items-center bg-orange z-0 pb-12 md:pb-16 lg:pb-20">
+    <div className="flex flex-col items-center bg-orange z-0 pt-12 pb-12 md:pb-16 lg:pb-20">
       <div className="flex items-baseline space-x-4 mb-4">
         <h1 className="text-white text-3xl font-medium">Upcoming</h1>
         <Heading classes="text-white text-[2.25rem]" underlineType={UnderlineTypes.PURPLE_SHORT_UNDERLINE}>
