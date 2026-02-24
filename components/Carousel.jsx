25a6202ed32d068bace02c18d8314f97736ee01e
Abi Debebe <abitibebu123@gmail.com>
2026-02-24 16:46:58 -0700

    import errors fixed and shadow removed

--- components/Carousel.jsx
+++ components/Carousel.jsx
@@ -1,5 +1,5 @@
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination } from "swiper";
+import { Navigation, Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/effect-cards";
 import "swiper/css/pagination";
